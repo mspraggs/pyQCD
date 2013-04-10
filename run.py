4c299dd6a27e869a992f108575f38bf489ea47d7
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-10 17:28:05 +0100

    Added notifications of progress of run script

--- run.py
+++ run.py
@@ -6,11 +6,13 @@ L = lattice.Lattice()
 #Thermalize the lattice
 
 for i in xrange(5*L.Ncor):
+    print("Thermalizing: %d" % i)
     L.update()
 
 Ps = np.zeros(L.Ncf)
     
 for i in xrange(L.Ncf):
     for j in xrange(L.Ncor):
+        print("Configuration: %d; Update: %d" % (i,j))
         L.update()
     Ps[i] = L.Pav()
