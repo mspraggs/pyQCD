e7c4d6f0cd2453d6e865e9ecefafd9dd6509346f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-13 00:41:41 +0100

    Added initialisation of links variable

--- lib/lattice.cpp
+++ lib/lattice.cpp
@@ -2,6 +2,7 @@
 #include <complex>
 #include <boost/python.hpp>
 using namespace Eigen;
+using namespace boost::python;
 
 class Lattice
 {
@@ -13,7 +14,7 @@ public:
 	  const double eps = 0.24);
 
   ~Lattice();
-  double P(const int site[4], const in mu, const int nu);
+  double P(const int site[4], const int mu, const int nu);
   double Pav();
   double Si(const int link[5]);
   Matrix3cd randomSU3();
@@ -26,14 +27,38 @@ private:
   
 };
 
-Lattice::Lattice(const int n = 8,
-		 const double beta = 5.5,
-		 const int Ncor = 50,
-		 const int Ncf = 1000,
-		 const double eps = 0.24)
+Lattice::Lattice(const int n, const double beta, const int Ncor, const int Ncf, const double eps)
 {
   /*Default constructor. Assigns function arguments to member variables
    and initializes links.*/
+  this->n = n;
+  this->beta = beta;
+  this->Ncor = Ncor;
+  this->Ncf = Ncf;
+  this->eps = eps;
+
+  //Initialize the array of matrices
+  this->links = new Matrix3cd**** [n];
+
+  for(int i = 0; i < n; i++) {
+    this->links[i] = new Matrix3cd*** [n];
+    
+    for(int j = 0; j < n; j++) {
+      this->links[i][j] = new Matrix3cd** [n];
+      
+      for(int k = 0; k < n; k++) {
+	this->links[i][j][k] = new Matrix3cd* [n];
+
+	for(int l = 0; l < n; l++) {
+	  this->links[i][j][k][l] = new Matrix3cd[4];
+
+	  for(int m = 0; m < 4; m++) {
+	    this->links[i][j][k][l][m] = Matrix3cd::Identity();
+	  }	  
+	}
+      }
+    }
+  }
 }
 
 Lattice::~Lattice()
@@ -43,6 +68,5 @@ Lattice::~Lattice()
 
 BOOST_PYTHON_MODULE(lattice)
 {
-  PyEval_InitThread();
-  class_<Lattice>("Lattice", init<optional<int,double,int,int,double> >())
+  class_<Lattice>("Lattice", init<optional<int,double,int,int,double> >());
 }
