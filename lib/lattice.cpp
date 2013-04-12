abafaeeffa9daf83eb8cfedee2a847acf9d61779
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-12 23:30:37 +0100

    Added boost python code to create a python module

--- lib/lattice.cpp
+++ lib/lattice.cpp
@@ -1,5 +1,6 @@
 #include <Eigen/Dense>
 #include <complex>
+#include <boost/python.hpp>
 using namespace Eigen;
 
 class Lattice
@@ -21,7 +22,7 @@ public:
 private:
   int n, Ncor, Ncf;
   double beta, eps;
-  Matrix3cd ***** links;
+  Matrix3cd***** links;
   
 };
 
@@ -29,7 +30,19 @@ Lattice::Lattice(const int n = 8,
 		 const double beta = 5.5,
 		 const int Ncor = 50,
 		 const int Ncf = 1000,
-		 const double eps = 0.24) {
+		 const double eps = 0.24)
+{
   /*Default constructor. Assigns function arguments to member variables
    and initializes links.*/
+}
+
+Lattice::~Lattice()
+{
+  /*Destructor*/
+}
+
+BOOST_PYTHON_MODULE(lattice)
+{
+  PyEval_InitThread();
+  class_<Lattice>("Lattice", init<optional<int,double,int,int,double> >())
 }
