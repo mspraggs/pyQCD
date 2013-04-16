8d7b0e4930c570fdd8778514c7af6437cc2b6340
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-17 00:03:26 +0100

    Added code to test speed of Wilson loop function

--- lib/test.py
+++ lib/test.py
@@ -2,8 +2,14 @@ import lattice
 import time
 
 L = lattice.Lattice()
+print("Timing update function")
 t1 = time.time()
 L.update()
 t2 = time.time()
-
 print("Run time = %f" % (t2-t1))
+print("Timing 1000 Wilson loop functions")
+t1 = time.time()
+for i in xrange(1000): L.W([0,0,0,0],[5,0,0,5])
+t2 = time.time()
+print("Run time = %f" % (t2-t1))
+
