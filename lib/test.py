bb39d17d826b2755bf1c21a1c238c3bc17e68793
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-18 01:02:55 +0100

    Updated W function call to reflect change in module

--- lib/test.py
+++ lib/test.py
@@ -10,13 +10,13 @@ print("Run time = %f" % (t2-t1))
 
 print("Timing 1000 5x5 Wilson loop functions")
 t1 = time.time()
-for i in xrange(1000): L.W([0,0,0,0],[5,0,0,5])
+for i in xrange(1000): L.W([0,0,0,0],5,5,3)
 t2 = time.time()
 print("Run time = %f" % (t2-t1))
 
 print("Timing 1000 1x1 Wilson loop functions")
 t1 = time.time()
-for i in xrange(1000): L.W([0,0,0,0],[1,0,0,1])
+for i in xrange(1000): L.W([0,0,0,0],1,1,3)
 t2 = time.time()
 print("Run time = %f" % (t2-t1))
 
