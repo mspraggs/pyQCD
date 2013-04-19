5689a6245145b8cadd8ec8a7a9d39051e8354e5c
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-19 23:08:15 +0100

    Added bootstrap, bin and calcaV functions and wrote script to calculate aV values with errors

--- postprocess.py
+++ postprocess.py
@@ -1,25 +1,68 @@
-import numpy as np
 from os import listdir,system
 from os.path import isfile, join
 import sys
+import IPython
+import pylab as pl
+from numpy import load
 
-files = [f for f in listdir("results") if isfile(join("results",f)) and f[-4:] == ".npy"]
+def bin(Ws,binsize=1):
+    """Split Ws into bins and return the average of each bin"""
+    if binsize == 1:
+        return Ws;
+    else:
+        extra = 0 if pl.size(Ws,axis=0) % binsize == 0 else 1
+        dims = [i for i in pl.shape(Ws)]
+        dims[0] = dims[0] / binsize + extra
+        dims = tuple(dims)
+        W_binned = pl.zeros(dims)
 
-files.sort()
+        for i in xrange(pl.size(W_binned,axis=0)):
+            W_binned[i] = pl.mean(Ws[i*binsize:(i+1)*binsize],axis=0)
 
-if len(files) == 0:
-    print("No data available.")
-    sys.exit()
+        return W_binned
 
-print("Available data:")
-for i in xrange(len(files)):
-    print("(%d) %s" % (i,files[i]))
+def calcaV(W):
+    """Calculate aV"""
+    return pl.log(pl.absolute(W/pl.roll(W,-1,axis=1)))
 
-file_num = input("File: ")
-filename = "results/%s" % files[file_num]
+def bootstrap(Ws):
+    """Bootstraps Ws N times and returns the average"""
+    Ws_bstrp = Ws[pl.randint(0,pl.size(Ws,axis=0),pl.size(Ws,axis=0))]
+    return Ws_bstrp
 
-Ws = np.load(filename)
+if __name__ == "__main__":
 
-Ncf,Nr,Nt = np.shape(Ws)
+    files = [f for f in listdir("results") if isfile(join("results",f)) and f[-4:] == ".npy"]
 
-aVs = np.log(Ws/np.roll(Ws,-1,axis=2))
+    files.sort()
+    
+    if len(files) == 0:
+        print("No data available.")
+        sys.exit()
+    
+    print("Available data:")
+    for i in xrange(len(files)):
+        print("(%d) %s" % (i,files[i]))
+        
+    file_num = input("File: ")
+    filename = "results/%s" % files[file_num]
+    Ws = load(filename)
+
+    N_bstrp = 10000
+    aVs = pl.zeros((N_bstrp,) + pl.shape(Ws)[1:])
+
+    for i in xrange(N_bstrp):
+        W = pl.mean(bootstrap(Ws),axis=0)
+        aVs[i] = calcaV(W)
+
+    t = pl.arange(1,7)
+    aV = pl.mean(aVs,axis=0)
+    e = pl.std(aVs,axis=0)
+
+    for i in xrange(len(t)):
+        pl.errorbar(t,aV[i],yerr=e[i],fmt='o',label=str(i))
+
+    IPython.embed()
+
+    pl.legend()
+    pl.show()
