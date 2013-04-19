8add4cbcea8dc760bfcb26495eddaf8f2e679353
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-19 13:37:05 +0100

    Changed to numpy array for Ws and added file output code

--- run.py
+++ run.py
@@ -1,23 +1,30 @@
 import lib.lattice as lattice
+import interfaces
 import numpy as np
 import fileio
 import sys
+import time
+import datetime
+from os.path import join
 
 L = lattice.Lattice()
 
 #Thermalize the lattice
-
 print("Thermalizing...")
 sys.stdout.flush()
 L.thermalize()
 print("Done!")
 sys.stdout.flush()
 
-Ps = [0] * L.Ncf
+rmax = 7
+tmax = 7
+Ws = np.zeros((L.Ncf,rmax-1,tmax-1))
     
 for i in xrange(L.Ncf):
-    L.nextConfig()
     print("Configuration: %d" % i)
-    Ps[i] = L.Pav()
+    L.nextConfig()
+    Ws[i] = interfaces.calcWs(L,rmax,tmax)
 
-fileio.writedata("plaquettes.txt",[Ps])
+time = datetime.datetime.now()
+filename = join("results","results_%s.npy" % time.strftime("%H:%M:%S_%d-%m-%Y"))
+np.save(filename,Ws)
