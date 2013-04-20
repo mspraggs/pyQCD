284faaa26445656d90222080051968db01cb993f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-20 12:09:33 +0100

    Added curve fitting code

--- postprocess.py
+++ postprocess.py
@@ -4,6 +4,7 @@ import sys
 import IPython
 import pylab as pl
 from numpy import load
+from scipy import optimize
 
 def bin(Ws,binsize=1):
     """Split Ws into bins and return the average of each bin"""
@@ -20,6 +21,27 @@ def bin(Ws,binsize=1):
             W_binned[i] = pl.mean(Ws[i*binsize:(i+1)*binsize],axis=0)
 
         return W_binned
+
+def V(b,r):
+    """Calculates the potential as b[0]*r - b[1]/r + b[2]"""
+    return b[0]*r - b[1]/r + b[2]
+
+def efunc(b,r,y):
+    """Calculates the error between data and curve"""
+    return y - V(b,r)
+
+def potfit(data):
+    """Extracts the three paramaeters for the potential
+    function from data"""
+
+    r = pl.arange(1,len(data)+1)
+    b0 = [1.,1.,1.]
+    b,result = optimize.leastsq(efunc,b0,args=(r,data))
+
+    if result != 1:
+        print("Warning! Fit failed.")
+
+    return b
 
 def calcaV(W):
     """Calculate aV"""
@@ -57,14 +79,24 @@ if __name__ == "__main__":
         W = pl.mean(bootstrap(Ws),axis=0)
         aVs[i] = calcaV(W)
 
-    t = pl.arange(1,7)
+    r = pl.arange(1,7)
     aV = pl.mean(aVs,axis=0)
     e = pl.std(aVs,axis=0)
 
-    for i in xrange(len(t)):
-        pl.errorbar(t,aV[i],yerr=e[i],fmt='o',label=str(i))
+    b = potfit(aV[:,0])
 
-    IPython.embed()
+    print("Fit parameters:")
+    print("sigma = %f" % b[0])
+    print("b = %f" % b[1])
+    print("c = %f" % b[2])
 
-    pl.legend()
+    r_fit = pl.arange(0.25,r[-1]+1,0.1)
+    aV_fit = V(b,r_fit)
+       
+    pl.errorbar(r,aV[:,0],fmt='ok')
+    pl.plot(r_fit,aV_fit,'r--')
+    pl.ylim([0,pl.nanmax(aV)+0.25])
+    pl.xlim([0,pl.nanmax(r_fit)+0.25])
+    pl.xlabel("$r / a$")
+    pl.ylabel("$aV(r)$")    
     pl.show()
