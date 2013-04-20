1e1d8740b213a8a8100a729dcabe060e814f4c55
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-20 12:10:15 +0100

    Updated calcWs to use new Wav function

--- interfaces.py
+++ interfaces.py
@@ -23,12 +23,9 @@ def getLinks(lattice):
 def calcWs(lattice,rmax,tmax,n_smears=0):
     """Calculates a series of Wilson loops up to the maximum r and t values"""
     out = np.zeros((rmax-1,tmax-1))
-
-    indices = [(i,j,k,l,m) for i in range(lattice.n) for j in range(lattice.n) for k in range(lattice.n) for l in range(lattice.n) for m in range(1,4)]
-
+    
     for r in xrange(1,rmax):
         for t in xrange(1,tmax):
-            for i,j,k,l,m in indices:
-                out[r-1,t-1] += lattice.W([i,j,k,l],r,t,m,n_smears)
+            out[r-1,t-1] += lattice.Wav(r,t,n_smears)
             
-    return out / len(indices)
+    return out
