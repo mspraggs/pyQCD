56768ee0de8e46ffcea09f5e71ed1ef962425ab6
Matt Spraggs <matthew.spraggs@gmail.com>
2013-04-22 17:39:15 +0100

    Added pickling support. Had to add an action variable in to get the copying of the action to play nicely

--- lib/lattice.cpp
+++ lib/lattice.cpp
@@ -71,14 +71,16 @@ public:
   Matrix3cd link(const int link[5]);
   void smear(const int time, const int n_smears);
   Matrix3cd fdiff(const int link[5]);
-  py::list getLink(const int i, const int j, const int k, const int l, const int m);
-  py::list getRandSU3(const int i);
+  py::list getLink(const int i, const int j, const int k, const int l, const int m) const;
+  py::list getRandSU3(const int i) const;
 
   int Ncor, Ncf, n;
 
+  friend struct my_pickle_suite;
+
 private:
   double beta, eps, a, u0, smear_eps;
-  int nupdates;
+  int nupdates, action;
   double SiW(const int link[5]);
   double SiImp(const int link[5]);
   vector< vector< vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > > > links;
@@ -102,6 +104,7 @@ Lattice::Lattice(const int n, const double beta, const int Ncor, const int Ncf, 
   this->smear_eps = smear_eps;
   this->nupdates = 0;
   this->u0 = u0;
+  this->action = action;
 
   srand(time(NULL));
   //Resize the link vector and assign each link a random SU3 matrix
@@ -157,6 +160,7 @@ Lattice::Lattice(const Lattice& L)
   this->links = L.links;
   this->randSU3s = L.randSU3s;
   this->Si = L.Si;
+  this->action = action;
 }
 
 double Lattice::init_u0()
@@ -717,7 +721,7 @@ void Lattice::printL()
   }
 }
 
-py::list Lattice::getLink(const int i, const int j, const int k, const int l, const int m)
+py::list Lattice::getLink(const int i, const int j, const int k, const int l, const int m) const
 {
   /*Returns the given link as a python nested list. Used in conjunction
    with python interfaces library to extract the links as a nested list
@@ -733,7 +737,7 @@ py::list Lattice::getLink(const int i, const int j, const int k, const int l, co
   return out;
 }
 
-py::list Lattice::getRandSU3(const int i)
+py::list Lattice::getRandSU3(const int i) const
 {
   /*Returns the given random SU3 matrix as a python list*/
   py::list out;
@@ -747,6 +751,101 @@ py::list Lattice::getRandSU3(const int i)
   return out;
 }
 
+struct my_pickle_suite : py::pickle_suite
+{
+  static py::tuple getinitargs(const Lattice& L)
+  {
+    return py::make_tuple(L.n,L.beta,L.Ncor,L.Ncf,L.eps,L.a,L.smear_eps,L.u0,L.action);
+  }
+  
+  static py::tuple getstate(const Lattice& L)
+  {
+    //Convert the links vector to a list for python compatability
+    py::list links;
+    for(int i = 0; i < L.n; i++) {
+      py::list temp1;
+      for(int j = 0; j < L.n; j++) {
+	py::list temp2;
+	for(int k = 0; k < L.n; k++) {
+	  py::list temp3;
+	  for(int l = 0; l < L.n; l++) {
+	    py::list temp4;
+	    for(int m = 0; m < 4; m++) {
+	      temp4.append(L.getLink(i,j,k,l,m));
+	    }
+	    temp3.append(temp4);
+	  }
+	  temp2.append(temp3);
+	}
+	temp1.append(temp2);
+      }
+      links.append(temp1);
+    }
+    
+    //Same for the randSU3s
+    py::list randSU3s;
+    for(int i = 0; i < 200; i++) {
+      randSU3s.append(L.getRandSU3(i));
+    }
+    return py::make_tuple(links,randSU3s);
+  }
+  
+  static void setstate(Lattice& L, py::tuple state)
+  {
+    if(len(state) != 2) {
+      PyErr_SetObject(PyExc_ValueError,
+		      ("expected 2-item tuple in call to __setstate__; got %s"
+		       % state).ptr());
+      py::throw_error_already_set();
+    }
+    
+    vector< vector< vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > > > links;
+    vector<Matrix3cd, aligned_allocator<Matrix3cd> > randSU3s;
+    py::list link_states = py::extract<py::list>(state[0]);
+    py::list randSU3_states = py::extract<py::list>(state[1]);
+    
+    //Convert the compound list of links back to a vector...
+    for(int i = 0; i < L.n; i++) {
+      vector< vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > > temp1;
+      for(int j = 0; j < L.n; j++) {
+	vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > temp2;
+	for(int k = 0; k < L.n; k++) {
+	  vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > temp3;
+	  for(int l = 0; l < L.n; l++) {
+	    vector<Matrix3cd, aligned_allocator<Matrix3cd> > temp4;
+	    for(int m = 0; m < 4; m++) {
+	      Matrix3cd temp_mat;
+	      for(int n = 0; n < 3; n++) {
+		for(int o = 0; o < 3; o++) {
+		  complex<double> temp_complex = py::extract<complex<double> >(link_states[i][j][k][l][m][n][o]);
+		  temp_mat(n,o) = temp_complex;
+		}
+	      }
+	      temp4.push_back(temp_mat);
+	    }
+	    temp3.push_back(temp4);
+	  }
+	  temp2.push_back(temp3);
+	}
+	temp1.push_back(temp2);
+      }
+      links.push_back(temp1);
+    }
+    //And the same for the random SU3 matrices.
+    for(int i = 0; i < 200; i++) {
+      Matrix3cd temp_mat;
+      for(int j = 0; j < 3; j++) {
+	for(int k = 0; k < 3; k++) {
+	  temp_mat(j,k) = py::extract<complex<double> >(randSU3_states[i][j][k]);
+	}
+      }
+      randSU3s.push_back(temp_mat);
+    }
+    L.links = links;
+    L.randSU3s = randSU3s;
+  }
+};
+  
 //Boost python wrapping of the class
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LatticeWOverload,W_p,4,5)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(LatticeWavOverload,Wav,2,3)
@@ -755,6 +854,7 @@ BOOST_PYTHON_MODULE(lattice)
 {
   py::class_<Lattice>("Lattice", py::init<py::optional<int,double,int,int,double,double,double,double,int> >())
     .def(py::init<Lattice&>())
+    .def_pickle(my_pickle_suite())
     .def("update",&Lattice::update)
     .def("nextConfig",&Lattice::nextConfig)
     .def("thermalize",&Lattice::thermalize)
