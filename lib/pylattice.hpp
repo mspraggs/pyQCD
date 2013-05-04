6ea9a1c4fd45010f39b698cc2d83860f835be7f7
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-04 22:42:26 +0100

    Updated naming conventions to match C++ conventions

--- lib/pylattice.hpp
+++ lib/pylattice.hpp
@@ -8,23 +8,29 @@ class pyLattice: public Lattice
 {
   friend struct lattice_pickle_suite;
 public:
-  pyLattice(const int n = 8,
-	  const double beta = 5.5,
-	  const int Ncor = 50,
-	  const int Ncf = 1000,
-	  const double eps = 0.24,
-	  const double a = 0.25,
-	  const double rho = 0.3,
-	  const double u0 = 1,
-	  const int action = 0);
+  pyLattice(const int nEdgePoints = 8,
+	    const double beta = 5.5,
+	    const int nCorrelations = 50,
+	    const int nConfigurations = 1000,
+	    const double epsilon = 0.24,
+	    const double a = 0.25,
+	    const double rho = 0.3,
+	    const double u0 = 1,
+	    const int action = 0);
+  pyLattice(const pyLattice& pylattice);
   ~pyLattice();
 
-  double calcTwistRect_p(const py::list site2,const int mu, const int nu);
-  double calcRectangle_p(const py::list site2,const int mu, const int nu);
-  double calcPlaquette_p(const py::list site2,const int mu, const int nu);
-  double calcWilsonLoop_p(const py::list cnr, const int r, const int t, const int dim, const int n_smears = 0);
-  double calcAverageWilson_p(const int r, const int t, const int n_smears = 0);
-  void runThreads(const int size, const int n_updates, const int remainder);
-  py::list getLink_p(const int i, const int j, const int k, const int l, const int m) const;
-  py::list getRandSU3(const int i) const;
+  double computePlaquetteP(const py::list site2,const int mu, const int nu);
+  double computeRectangleP(const py::list site2,const int mu, const int nu);
+  double computeTwistedRectangleP(const py::list site2, const int mu,
+				   const int nu);
+  double computeWilsonLoopP(const py::list cnr, const int r, const int t,
+			     const int dim, const int nSmears = 0);
+  double computeAverageWilsonLoopP(const int r, const int t,
+				    const int nSmears = 0);
+  void runThreads(const int chunkSize, const int nUpdates,
+		  const int remainder);
+  py::list getLinkP(const int n0, const int n1, const int n2, const int n3,
+		     const int dimension) const;
+  py::list getRandSu3(const int index) const;
 };
