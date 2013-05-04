27a9e09782cf0c600c6034603015b6a3f938a8eb
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-05 00:44:38 +0100

    Updated files in line with new naming conventions in pyLattice and Lattice

--- lib/pyQCD.cpp
+++ lib/pyQCD.cpp
@@ -4,29 +4,37 @@
 
 namespace py = boost::python;
 
-//Class that inherits Lattice and provides python wrapper functions
+// Class that inherits Lattice and provides python wrapper functions
 
 struct lattice_pickle_suite : py::pickle_suite
 {
-  static py::tuple getinitargs(const pyLattice& L)
+  static py::tuple getinitargs(const pyLattice& pylattice)
   {
-    return py::make_tuple(L.n,L.beta,L.Ncor,L.Ncf,L.eps,L.a,L.smear_eps,L.u0,L.action);
+    return py::make_tuple(pylattice.nEdgePoints,
+			  pylattice.beta_,
+			  pylattice.nCorrelations,
+			  pylattice.nConfigurations,
+			  pylattice.epsilon_,
+			  pylattice.a_,
+			  pylattice.rho_,
+			  pylattice.u0_,
+			  pylattice.action_);
   }
   
-  static py::tuple getstate(const pyLattice& L)
+  static py::tuple getstate(const pyLattice& pylattice)
   {
-    //Convert the links vector to a list for python compatability
+    // Convert the links vector to a list for python compatability
     py::list links;
-    for(int i = 0; i < L.n; i++) {
+    for (int i = 0; i < pylattice.nEdgePoints; i++) {
       py::list temp1;
-      for(int j = 0; j < L.n; j++) {
+      for (int j = 0; j < pylattice.nEdgePoints; j++) {
 	py::list temp2;
-	for(int k = 0; k < L.n; k++) {
+	for (int k = 0; k < pylattice.nEdgePoints; k++) {
 	  py::list temp3;
-	  for(int l = 0; l < L.n; l++) {
+	  for (int l = 0; l < pylattice.nEdgePoints; l++) {
 	    py::list temp4;
-	    for(int m = 0; m < 4; m++) {
-	      temp4.append(L.getLink(i,j,k,l,m));
+	    for (int m = 0; m < 4; m++) {
+	      temp4.append(pylattice.getLinkP(i, j, k, l, m));
 	    }
 	    temp3.append(temp4);
 	  }
@@ -37,51 +45,53 @@ struct lattice_pickle_suite : py::pickle_suite
       links.append(temp1);
     }
     
-    //Same for the randSU3s
-    py::list randSU3s;
+    // Same for the randSU3s
+    py::list randSu3s;
     int index = 0;
-    for(int i = 0; i < 20; i++) {
-      py::list temp_list;
-      for(int j = 0; j < 20; j++) {
-	temp_list.append(L.getRandSU3(index));
+    for (int i = 0; i < 20; i++) {
+      py::list tempList;
+      for (int j = 0; j < 20; j++) {
+	tempList.append(pylattice.getRandSu3(index));
 	index++;
       }
-      randSU3s.append(temp_list);
+      randSu3s.append(tempList);
     }
-    return py::make_tuple(links,randSU3s);
+    return py::make_tuple(links, randSu3s);
   }
   
-  static void setstate(pyLattice& L, py::tuple state)
+  static void setstate(pyLattice& pylattice, py::tuple state)
   {
-    if(len(state) != 2) {
+    if (len(state) != 2) {
       PyErr_SetObject(PyExc_ValueError,
-		      ("expected 2-item tuple in call to __setstate__; got %s"
-		       % state).ptr());
+        ("expected 2-item tuple in call to __setstate__; got %s"
+	 % state).ptr());
       py::throw_error_already_set();
     }
     
-    vector< vector< vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > > > links;
-    vector<Matrix3cd, aligned_allocator<Matrix3cd> > randSU3s;
-    py::list link_states = py::extract<py::list>(state[0]);
-    py::list randSU3_states = py::extract<py::list>(state[1]);
+    GaugeField links;
+    Sub4Field randSu3s;
+    py::list linkStates = py::extract<py::list>(state[0]);
+    py::list randSu3States = py::extract<py::list>(state[1]);
 
-    //Convert the compound list of links back to a vector...
-    for(int i = 0; i < L.n; i++) {
-      vector< vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > > temp1;
-      for(int j = 0; j < L.n; j++) {
-	vector< vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > > temp2;
-	for(int k = 0; k < L.n; k++) {
-	  vector< vector<Matrix3cd, aligned_allocator<Matrix3cd> > > temp3;
-	  for(int l = 0; l < L.n; l++) {
-	    vector<Matrix3cd, aligned_allocator<Matrix3cd> > temp4;
-	    for(int m = 0; m < 4; m++) {
-	      Matrix3cd temp_mat;
-	      for(int n = 0; n < 3; n++) {
-		for(int o = 0; o < 3; o++) {
-		  temp_mat(n,o) = py::extract<complex<double> >(link_states[i][j][k][l][m][n][o]);
+    // Convert the compound list of links back to a vector...
+    for (int i = 0; i < pylattice.nEdgePoints; i++) {
+      SubField temp1;
+      for (int j = 0; j < pylattice.nEdgePoints; j++) {
+	Sub2Field temp2;
+	for (int k = 0; k < pylattice.nEdgePoints; k++) {
+	  Sub3Field temp3;
+	  for (int l = 0; l < pylattice.nEdgePoints; l++) {
+	    Sub4Field temp4;
+	    for (int m = 0; m < 4; m++) {
+	      Matrix3cd tempMatrix;
+	      for (int n = 0; n < 3; n++) {
+		for (int o = 0; o < 3; o++) {
+		  tempMatrix(n, o) =
+		    py::extract<complex<double> >
+		    (linkStates[i][j][k][l][m][n][o]);
 		}
 	      }
-	      temp4.push_back(temp_mat);
+	      temp4.push_back(tempMatrix);
 	    }
 	    temp3.push_back(temp4);
 	  }
@@ -91,50 +101,60 @@ struct lattice_pickle_suite : py::pickle_suite
       }
       links.push_back(temp1);
     }
-    //And the same for the random SU3 matrices.
+    // And the same for the random SU3 matrices.
     int index = 0;
-    for(int i = 0; i < 20; i++) {
-      for(int j = 0; j < 20; j++) {
-	Matrix3cd temp_mat;
-	for(int k = 0; k < 3; k++) {
-	  for(int l = 0; l < 3; l++) {
-	    temp_mat(k,l) = py::extract<complex<double> >(randSU3_states[i][j][k][l]);
+    for (int i = 0; i < 20; i++) {
+      for (int j = 0; j < 20; j++) {
+	Matrix3cd tempMatrix;
+	for (int k = 0; k < 3; k++) {
+	  for (int l = 0; l < 3; l++) {
+	    tempMatrix(k, l) =
+	      py::extract<complex<double> >(randSu3States[i][j][k][l]);
 	  }
 	}
-	randSU3s.push_back(temp_mat);
+	randSu3s.push_back(tempMatrix);
       }
     }
     
-    L.links = links;
-    L.randSU3s = randSU3s;
+    pylattice.links_ = links;
+    pylattice.randSu3s_ = randSu3s;
   }
 };
   
-//Boost python wrapping of the class
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(pyLatticeWOverload,W_p,4,5)
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(pyLatticeWavOverload,Wav_p,2,3)
+// Boost python wrapping of the class
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(pyLatticeWOverload,
+				       computeWilsonLoopP, 4, 5)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(pyLatticeWavOverload, 
+				       computeAverageWilsonLoopP, 2, 3)
 
 BOOST_PYTHON_MODULE(pyQCD)
 {
-  py::class_<pyLattice>("Lattice", py::init<py::optional<int,double,int,int,double,double,double,double,int> >())
+  py::class_<pyLattice>("Lattice",
+			py::init<py::optional<int, double, int, int, double,
+					      double, double, double,
+					      int> >())
     .def(py::init<pyLattice&>())
+    .def("link", &pyLattice::getLinkP)
+    .def("update", &pyLattice::update)
+    .def("schwarz_update", &pyLattice::schwarzUpdate)
+    .def("next_config", &pyLattice::getNextConfig)
+    .def("thermalize", &pyLattice::thermalize)
+    .def("plaquette", &pyLattice::computePlaquetteP)
+    .def("rectangle", &pyLattice::computeRectangleP)
+    .def("twist_rect", &pyLattice::computeTwistedRectangleP)
+    .def("wilson_loop", &pyLattice::computeWilsonLoopP,
+	 pyLatticeWOverload(py::args("corner", "r", "t", "dimension",
+				     "nSmears"),
+			    "Calculate Wilson loop"))
+    .def("av_plaquette", &pyLattice::computeAveragePlaquette)
+    .def("av_rectangle", &pyLattice::computeAverageRectangle)
+    .def("av_wilson_loop", &pyLattice::computeAverageWilsonLoopP,
+	 pyLatticeWavOverload(py::args("r", "t", "nSmears"),
+			      "Calculate average Wilson loop"))
+    .def("print", &pyLattice::print)
+    .def("get_rand_su3", &pyLattice::getRandSu3)
     .def_pickle(lattice_pickle_suite())
-    .def("updateSchwarz",&pyLattice::updateSchwarz)
-    .def("update",&pyLattice::update)
-    .def("nextConfig",&pyLattice::nextConfig)
-    .def("thermalize",&pyLattice::thermalize)
-    .def("init_u0",&pyLattice::init_u0)
-    .def("P",&pyLattice::P_p)
-    .def("Pav",&pyLattice::Pav)
-    .def("R",&pyLattice::R_p)
-    .def("Rav",&pyLattice::Rav)
-    .def("T",&pyLattice::T_p)
-    .def("W",&pyLattice::W_p,pyLatticeWOverload(py::args("cnr","r","t","dim","n_smears"), "Calculate Wilson loop"))
-    .def("Wav",&pyLattice::Wav_p,pyLatticeWavOverload(py::args("r","t","n_smears"), "Calculate average Wilson loop"))
-    .def("printL",&pyLattice::printL)
-    .def("getLink",&pyLattice::getLink)
-    .def("getRandSU3",&pyLattice::getRandSU3)
-    .def_readonly("Ncor",&pyLattice::Ncor)
-    .def_readonly("Ncf",&pyLattice::Ncf)
-    .def_readonly("n",&pyLattice::n);
+    .def_readonly("n_cor", &pyLattice::nCorrelations)
+    .def_readonly("n_conf", &pyLattice::nConfigurations)
+    .def_readonly("n_points", &pyLattice::nEdgePoints);
 }
