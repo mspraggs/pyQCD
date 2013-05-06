1aff28a14222daf87fc9fb51fb4e098890d6e14f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-06 21:38:10 +0100

    Added private copy constructor and assignment operators to ensure non-copyable

--- lib/src/gil.cpp
+++ lib/src/gil.cpp
@@ -4,23 +4,26 @@
 
 using namespace boost::python;
 
-class ScopedGILRelease
+class ScopedGILRelease //: public boost::noncopyable
 {
-// C & D -------------------------------------------------------------------------------------------
+
 public:
-    inline ScopedGILRelease()
-    {
-        m_thread_state = PyEval_SaveThread();
-    }
-
-    inline ~ScopedGILRelease()
-    {
-        PyEval_RestoreThread(m_thread_state);
-        m_thread_state = NULL;
-    }
-
+  inline ScopedGILRelease()
+  {
+    m_thread_state = PyEval_SaveThread();
+  }
+  
+  inline ~ScopedGILRelease()
+  {
+    PyEval_RestoreThread(m_thread_state);
+    m_thread_state = NULL;
+  }
+  
 private:
-    PyThreadState * m_thread_state;
+  PyThreadState * m_thread_state;
+  
+  ScopedGILRelease(const ScopedGILRelease& scoped);
+  ScopedGILRelease& operator=(const ScopedGILRelease& scoped);
 };
 
 #endif
