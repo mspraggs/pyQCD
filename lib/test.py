be9c7a69f749bf196314e2e9fe45fdccf2be3b07
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-06 14:44:23 +0100

    Bringing up to date with function names in pyQCD.so

--- lib/test.py
+++ lib/test.py
@@ -17,24 +17,24 @@ for i in xrange(3):
     
     print("Timing Schwarz update function")
     t1 = time.time()
-    L.updateSchwarz(4,1)
+    L.schwarz_update(4,1)
     t2 = time.time()
     print("Run time = %f" % (t2-t1))
     
     print("Timing 1000 5x5 Wilson loop functions")
     t1 = time.time()
-    for i in xrange(1000): L.W([0,0,0,0],5,5,3)
+    for i in xrange(1000): L.wilson_loop([0,0,0,0],5,5,3)
     t2 = time.time()
     print("Run time = %f" % (t2-t1))
     
     print("Timing 1000 1x1 Wilson loop functions")
     t1 = time.time()
-    for i in xrange(1000): L.W([0,0,0,0],1,1,3)
+    for i in xrange(1000): L.wilson_loop([0,0,0,0],1,1,3)
     t2 = time.time()
     print("Run time = %f" % (t2-t1))
     
     print("Timing 1000 plaquette functions")
     t1 = time.time()
-    for i in xrange(1000): L.P([0,0,0,0],0,3)
+    for i in xrange(1000): L.plaquette([0,0,0,0],0,3)
     t2 = time.time()
     print("Run time = %f" % (t2-t1))
