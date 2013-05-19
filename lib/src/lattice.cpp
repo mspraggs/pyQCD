ae5875d6ce15d798f2b68c7af86a939a7b921f84
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-19 21:59:59 +0100

    Corrected number of collapsed for loops

--- lib/src/lattice.cpp
+++ lib/src/lattice.cpp
@@ -934,7 +934,7 @@ void Lattice::smearLinks(const int time, const int nSmears)
 		      Sub2Field(this->nEdgePoints,
 				Sub3Field(this->nEdgePoints, 
 					  Sub4Field(4))));
-#pragma omp parallel for collapse(4)
+#pragma omp parallel for collapse(3)
     for (int j = 0; j < this->nEdgePoints; ++j) {
       for (int k = 0; k < this->nEdgePoints; ++k) {
 	for (int l = 0; l < this->nEdgePoints; ++l) {
