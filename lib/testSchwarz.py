46616b2ad0a214ad507850cf162400685c03064d
Matt Spraggs <matthew.spraggs@gmail.com>
2013-05-25 23:59:18 +0100

    Added iteration counting to testing of schwarz update procedure

--- lib/testSchwarz.py
+++ lib/testSchwarz.py
@@ -3,20 +3,24 @@ import time
 
 L = pyQCD.Lattice()
 t1 = time.time()
+i1 = 0
 
 while L.av_plaquette() > 0.5:
-	L.schwarz_update(4,10)
+	L.schwarz_update(4,1)
 	print(L.av_plaquette())
+	i1 += 1
 
 t2 = time.time()
 L = pyQCD.Lattice()
 t3 = time.time()
+i2 = 0
 
 while L.av_plaquette() > 0.5:
 	L.update()
 	print(L.av_plaquette())
+	i2 += 1
 
 t4 = time.time()
 
-print("Schwarz convergence = %f seconds" % (t2-t1))
-print("Standard convergence = %f seconds" % (t4-t3))
+print("Schwarz convergence = %f seconds, %d iterations" % ((t2-t1), i1))
+print("Standard convergence = %f seconds, %d iterations" % ((t4-t3), i2))
