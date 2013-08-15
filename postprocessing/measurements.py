43f470e5699abd6aa3f69dd5fd0f1a5a22cf150f
Matt Spraggs <matthew.spraggs@gmail.com>
2013-08-16 00:46:26 +0100

    Altered measurement routines to accept inputs of different shapes.

--- postprocessing/measurements.py
+++ postprocessing/measurements.py
@@ -1,4 +1,6 @@
 import pylab as pl
+from scipy import optimize
+import IPython
 
 def pair_potential(b, r):
 	"""Calculates the quark pair potential as b[0] * r - b[1] / r + b[2]"""
@@ -7,31 +9,47 @@ def pair_potential(b, r):
 def potential_params(data):
 	"""Fits the potential function to data and returns the three fitting
 	parameters"""
-
-	r = pl.arange(1, pl.size(data) + 1)
-	b0 = pl.array([1.0, 1.0, 1.0])
-
-	err_func = lambda b, r, y: y - pair_potential(b, r)
+	if len(pl.shape(data)) == 1:
 	
-	b, result = optimize.leastq(err_func, b0, args = (r, data))
+		r = pl.arange(1, pl.size(data) + 1)
+		b0 = pl.array([1.0, 1.0, 1.0])
 
-	if result != 1:
-		print("Warning! Fit failed.")
+		err_func = lambda b, r, y: y - pair_potential(b, r)
+		b, result = optimize.leastsq(err_func, b0, args = (r, data))
 
-	return b
+		if result != 1:
+			print("Warning! Fit failed.")
+
+		return b
+
+	else:
+		b_store = pl.zeros((pl.size(data, axis = 0), 3))
+
+		for i in xrange(pl.size(data, axis = 0)):
+			b_store[i] = potential_params(data[i])
+
+		return b_store
 
 def calculate_potential(wilson_loops):
 	"""Calculate the pair potential from the average wilson loops provided"""
-	potentials = pl.zeros(pl.size(wilson_loops, axis = 0))
-	# The quark separations
-	r = pl.arange(1, pl.size(wilson_loops, axis = 1) + 1)
-	# The function we'll use to fit the Wilson loops
-	f = lambda b, t, W: W - b[0] * pl.exp(-b[1] * t)
+	if len(pl.shape(wilson_loops)) == 2:
+		potentials = pl.zeros(pl.size(wilson_loops, axis = 0))
+		# The quark separations
+		r = pl.arange(1, pl.size(wilson_loops, axis = 0) + 1)
+		t = pl.arange(1, pl.size(wilson_loops, axis = 1) + 1)
+		# The function we'll use to fit the Wilson loops
+		f = lambda b, t, W: W - b[0] * pl.exp(-b[1] * t)
+		
+		for i in xrange(pl.size(wilson_loops, axis = 0)):
+			params, result = optimize.leastsq(f, [1., 1.],
+											  args = (t, wilson_loops[i]))
+			potentials[i] = params[1]
 
-	for i in xrange(pl.size(wilson_loops, axis = 0)):
-		params, result = optimize.leastsq(f, [1., 1.],
-										  args = (n, wilson_loops[i]))
-		potentials[i] = params[1]
+	else:
+		potentials = pl.zeros(pl.shape(wilson_loops)[:-1])
+
+		for i in xrange(pl.size(wilson_loops, axis = 0)):
+			potentials[i] = calculate_potential(wilson_loops[i])
 
 	return potentials
 
@@ -52,4 +70,7 @@ def auto_correlation(plaquettes):
 def calculate_spacing(wilson_loops):
 	"""Calculates the lattice spacing using the Sommer scale"""
 	fit_params = potential_params(wilson_loops)
-	return 0.5 / pl.sqrt((1.65 + fit_params[1]) / fit_params[2])
+	if len(pl.shape(fit_params)) > 1:
+		return 0.5 / pl.sqrt((1.65 + fit_params[:,1]) / fit_params[:,0])
+	else:
+		return 0.5 / pl.sqrt((1.65 + fit_params[1]) / fit_params[0])		
