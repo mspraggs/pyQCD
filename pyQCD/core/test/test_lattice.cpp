6142e7c161122f9f2b69a2c14622ff54287eda46
Matt Spraggs <matthew.spraggs@gmail.com>
2013-08-24 17:40:41 +0100

    Added comments to test file.

--- pyQCD/core/test/test_lattice.cpp
+++ pyQCD/core/test/test_lattice.cpp
@@ -41,6 +41,7 @@ BOOST_AUTO_TEST_CASE( gluonic_measurements_test )
 
   int site[4] = {0, 0, 0, 0};
 
+  // Checking all plaquettes, rectangles and twisted rectangles
   for (int i = 1; i < 4; ++i) {
     for (int j = 0; j < i; ++j) {
       BOOST_CHECK_EQUAL(lattice.computePlaquette(site, i, j), 1.0);
@@ -48,63 +49,69 @@ BOOST_AUTO_TEST_CASE( gluonic_measurements_test )
       BOOST_CHECK_EQUAL(lattice.computeTwistedRectangle(site, i, j), 1.0);
     }
   }
-
+  // Checking average plaquette and rectangle
   BOOST_CHECK_EQUAL(lattice.computeAveragePlaquette(), 1.0);
   BOOST_CHECK_EQUAL(lattice.computeAverageRectangle(), 1.0);
-
+  // Checking average Wilson loops
   BOOST_CHECK_EQUAL(lattice.computeAverageWilsonLoop(1, 1), 1.0);
   BOOST_CHECK_EQUAL(lattice.computeAverageWilsonLoop(1, 1, 1, 0.5), 1.0);
 }
 
 BOOST_AUTO_TEST_CASE( update_test )
 {
+  int linkCoords[5] = {0, 0, 0, 0, 0};
+
+  // Checking parallel heatbath updates
   Lattice lattice(8, 8, 5.5, 1.0, 0, 10, 0, 1, 4);
   lattice.thermalize();
-      
+  // Check basic observables
   BOOST_CHECK(lattice.computeAveragePlaquette() < 0.51 &&
-	      lattice.computeAveragePlaquette() > 0.49);
-      
+	      lattice.computeAveragePlaquette() > 0.49);   
   BOOST_CHECK(lattice.computeAverageRectangle() < 0.27 &&
 	      lattice.computeAverageRectangle() > 0.25);
+  // Check unitarity
   Matrix3cd testLink = lattice.getLink(linkCoords);
   BOOST_CHECK(areEqual(testLink * testLink.adjoint(), Matrix3cd::Identity(),
 		       100 * DBL_EPSILON));
   BOOST_CHECK(areEqual(testLink.determinant(), 1.0, 100 * DBL_EPSILON));
 
+  // Checking parallel Monte Carlo updates
   lattice = Lattice(8, 8, 5.5, 1.0, 0, 60, 1, 1, 4);
   lattice.thermalize();
-      
+  // Check basic observables
   BOOST_CHECK(lattice.computeAveragePlaquette() < 0.51 &&
 	      lattice.computeAveragePlaquette() > 0.49);
-      
   BOOST_CHECK(lattice.computeAverageRectangle() < 0.27 &&
 	      lattice.computeAverageRectangle() > 0.25);
+  // Check unitarity
   testLink = lattice.getLink(linkCoords);
   BOOST_CHECK(areEqual(testLink * testLink.adjoint(), Matrix3cd::Identity(),
 		       100 * DBL_EPSILON));
   BOOST_CHECK(areEqual(testLink.determinant(), 1.0, 100 * DBL_EPSILON));
 
+  // Checking serial heatbath
   lattice = Lattice(8, 8, 5.5, 1.0, 0, 10, 0, 0, 4);
   lattice.thermalize();
-      
+  // Check basic observables
   BOOST_CHECK(lattice.computeAveragePlaquette() < 0.51 &&
 	      lattice.computeAveragePlaquette() > 0.49);
-      
   BOOST_CHECK(lattice.computeAverageRectangle() < 0.27 &&
 	      lattice.computeAverageRectangle() > 0.25);
+  // Check unitarity
   testLink = lattice.getLink(linkCoords);
   BOOST_CHECK(areEqual(testLink * testLink.adjoint(), Matrix3cd::Identity(),
 		       100 * DBL_EPSILON));
   BOOST_CHECK(areEqual(testLink.determinant(), 1.0, 100 * DBL_EPSILON));
 
+  // Checking serial Monte Carlo
   lattice = Lattice(8, 8, 5.5, 1.0, 0, 60, 1, 0, 4);
   lattice.thermalize();
-      
+  // Check basic observables
   BOOST_CHECK(lattice.computeAveragePlaquette() < 0.51 &&
 	      lattice.computeAveragePlaquette() > 0.49);
-      
   BOOST_CHECK(lattice.computeAverageRectangle() < 0.27 &&
 	      lattice.computeAverageRectangle() > 0.25);
+  // Check unitarity
   testLink = lattice.getLink(linkCoords);
   BOOST_CHECK(areEqual(testLink * testLink.adjoint(), Matrix3cd::Identity(),
 		       100 * DBL_EPSILON));
