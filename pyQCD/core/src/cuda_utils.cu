f16049d1daa29904a79d349a4b3b62ffc40e047a
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-03 15:15:21 +0100

    Tidied up existing cuda code.

--- pyQCD/core/src/cuda_utils.cu
+++ pyQCD/core/src/cuda_utils.cu
@@ -3,7 +3,7 @@
 namespace pyQCD
 {
   namespace cuda
-  {
+  {  
     void createSource(const int spatialIndex, const int spin, const int colour,
 		      const complexHybridDev& smearingOperator,
 		      cusp::array1d<cusp::complex<float>, devMem>& source,
@@ -25,60 +25,55 @@ namespace pyQCD
 		  const int spatialIndex,
 		  cusp::array2d<cusp::complex<float>, hostMem>& propagator)
     {
+      // Get the size of the Dirac matrix
       int nCols = hostDirac.num_cols;
-
-      std::cout << "Managed to get number of matrix cols" << std::endl;
-      
+      // Transfer the Dirac and smearing matrices to the device.
       complexHybridDev devDirac = hostDirac;
-      std::cout << "Transferred Dirac matrix" << std::endl;
       complexHybridDev devSourceSmear = hostSourceSmear;
       complexHybridDev devSinkSmear = hostSinkSmear;
 
-      std::cout << "Managed to transfer matrices to device" << std::endl;
+      // Create some device arrays to hold the source and solution
+      cusp::array1d<cusp::complex<float>, devMem>
+	source(nCols, cusp::complex<float>(0, 0));
+      cusp::array1d<cusp::complex<float>, devMem>
+	solution(nCols, cusp::complex<float>(0, 0));
 
-      cusp::array1d<cusp::complex<float>,
-		    devMem> source(nCols, cusp::complex<float>(0, 0));
-      cusp::array1d<cusp::complex<float>,
-		    devMem> solution(nCols, cusp::complex<float>(0, 0));
-
-      cusp::array1d<cusp::complex<float>,
-		    devMem> tempSource(nCols, cusp::complex<float>(0, 0));
+      // And another load as we'll need duplicates for the
+      // multiplication routines
+      cusp::array1d<cusp::complex<float>, devMem>
+	tempSource(nCols, cusp::complex<float>(0, 0));
       cusp::array1d<cusp::complex<float>, devMem>
 	tempSolution(nCols, cusp::complex<float>(0, 0));
 
-      cusp::array1d<cusp::complex<float>, devMem>::view
-	solutionView = cusp::make_array1d_view(tempSolution);
-
+      // Create a temporary propagator variable for fast access
       cusp::array2d<cusp::complex<float>, devMem>
 	tempPropagator(nCols, 12, cusp::complex<float>(0, 0));
 
+      // Loop through all spins and colours and do the inversions
       for (int i = 0; i < 4; ++i) {
 	for (int j = 0; j < 3; ++j) {
+	  // Create the source using the smearing operator
 	  createSource(spatialIndex, i, j, devSourceSmear, source,
 		       tempSource);
-	  //tempSource[j + 3 * (i + spatialIndex)]
-	  //  = cusp::complex<float>(0, 0);
-
-	  cusp::verbose_monitor<cusp::complex<float> > monitor(source, 100,
-							       1e-3);
+	  // Set up the monitor for use in the solver
+	  cusp::verbose_monitor<cusp::complex<float> >
+	    monitor(source, 100, 1e-3);
+	  // Create the preconditioner
 	  cusp::identity_operator<cusp::complex<float>, devMem>
 	    preconditioner(devDirac.num_rows, devDirac.num_rows);
-
+	  // Do the inversion
 	  cusp::krylov::bicgstab(devDirac, solution, source, monitor,
 				 preconditioner);
-       
+	  // Smear at the sink
 	  cusp::multiply(devSinkSmear, solution, tempSolution);
-
+	  // Create a view to the relevant column of the propagator
 	  cusp::array2d<cusp::complex<float>, devMem>::column_view
 	    propagatorView = tempPropagator.column(j + 3 * i);
-	  cusp::copy(tempSolution, propagatorView);// = tempSolution;
-	  //cusp::convert(solutionView, tempPropagator.column(j + 3 * i));
-	  //tempPropagator.column(j + 3 * i) = solutionView;
-	  //for (int k = 0; k < nCols; ++k) {
-	  //  tempPropagator(k, j + 3 * i) = tempSolution[k];
-	  //}
+	  // Copy the solution to the propagator output
+	  cusp::copy(tempSolution, propagatorView);
 	}
       }
+      // Move the propagator back into main memory
       propagator = tempPropagator;
     }
 
