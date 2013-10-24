2dd38a25ac61b38abc6cc4eefc4c82200fb1fbef
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-24 17:56:28 +0100

    Added option of function args and corrected shaping of measurement arrays in measurement bootstrapping.

--- pyQCD/postprocessing/statistics.py
+++ pyQCD/postprocessing/statistics.py
@@ -21,7 +21,7 @@ def bootstrap(X):
     """Performs a bootstrap resampling of X."""
     return X[pl.randint(0, pl.size(X, axis = 0), pl.size(X, axis = 0))]
 
-def bootstrap_measurement(X, func, num_bootstraps, binsize):
+def bootstrap_measurement(X, func, num_bootstraps, binsize, args = []):
     """Calculates bootstrap statistics for a given measurement function."""
     out = []
 
@@ -31,16 +31,25 @@ def bootstrap_measurement(X, func, num_bootstraps, binsize):
     if num_bootstraps > 1:
         for i in xrange(num_bootstraps):
             X_bootstrap = bootstrap(X)
-            measurement = func(X_bootstrap)
+            measurement = func(X_bootstrap, *args)
+            old_shape = list(measurement.shape[1:])
             measurement = measurement[pl.logical_not(pl.isnan(measurement))]
+            dim1_size = measurement.size / pl.product(old_shape)
+            new_shape = tuple([dim1_size] + old_shape)
+            measurement = pl.reshape(measurement, new_shape)
             out.append(pl.mean(measurement, axis=0))
     else:
-        measurement = func(X)
+        measurement = func(X, *args)
+        old_shape = list(measurement.shape[1:])
         measurement = measurement[pl.logical_not(pl.isnan(measurement))]
+        dim1_size = measurement.size / pl.product(old_shape)
+        new_shape = tuple([dim1_size] + old_shape)
+        measurement = pl.reshape(measurement, new_shape)
         out.append(measurement)
         
     out = pl.array(out) if len(out) > 1 else pl.array(out[0])
 
+    print(out)
     out_mean = pl.mean(out, axis=0)
     out_std = pl.std(out, axis=0)
 
