044f4c2f7bbcb02778187259c5d08b21f0ec2e8c
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-30 21:37:17 +0000

    Fleshed out config constructor and save methods.

--- pyQCD/core/config.py
+++ pyQCD/core/config.py
@@ -1,18 +1,39 @@
+import numpy as np
 
 class Config:
     
-    def __init__(self):
-        pass
+    def __init__(self, links, L, T, beta, u0, action, n_cor, update_method,
+                 parallel_updates, block_size, rand_seed):
+        """Create a configuration container."""
+        
+        expected_shape = (T, L, L, L, 4, 3, 3)
+        if links.shape != expected_shape:
+            raise ValueError("Shape of specified links array, {}, does not "
+                             "match the specified lattice extents, {}"
+                             .format(links.shape, expected_shape))
+        
+        self.L = L
+        self.T = T
+        self.beta = beta
+        self.u0 = u0
+        self.action = action
+        self.n_cor = n_cor
+        self.update_method = update_method
+        self.parallel_updates = parallel_updates
+        self.block_size = block_size,
+        self.rand_seed = rand_seed
+        
+        self.data = links
     
     def save(self, filename):
-        pass
-    
-    def __str__(self):
-        pass
-    
-    def av_plaquette(self):
-        pass
-
+        """Documentation"""
+        keys = ['L', 'T', 'beta', 'u0', 'action', 'n_cor', 'update_method',
+                'parallel_updates', 'block_size', 'rand_seed']
+        
+        items = [getattr(self, key) for key in keys]
+        
+        np.savez(filename, header = dict(zip(keys, items)), data = self.data)
+        
     @classmethod
     def load(self, filename):
         pass
