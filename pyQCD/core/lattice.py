a9bee8dfa28751e45ba396549cfa3645d1601175
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-30 18:07:19 +0000

    Fleshed out lattice constructor.

--- pyQCD/core/lattice.py
+++ pyQCD/core/lattice.py
@@ -1,7 +1,26 @@
 from kernel import lattice
-import wilslp, propagator, config, header
+import wilslps, propagator, config
+import dicts
 
 class Lattice(lattice.Lattice):
+    
+    def __init__(self, L = 4, T = 8, beta = 5.5, u0 = 1.0, action = "wilson",
+                 n_cor = 10, update_method = "heatbath", parallel_updates = True,
+                 block_size = 4, rand_seed = -1):
+        
+        self.beta = beta
+        self.u0 = u0
+        self.action = action
+        self.update_method = update_method
+        self.parallel_updates = parallel_updates
+        self.block_size = block_size
+        self.rand_seed = rand_seed
+        
+        lattice.Lattice.__init__(self, L, T, beta, u0,
+                                 dicts.gauge_actions[action], n_cor,
+                                 dicts.update_methods[update_method],
+                                 dicts.truefalse[parallel_updates], block_size,
+                                 rand_seed)
     
     def get_config(self):
         out = config.Config()
