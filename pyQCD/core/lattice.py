e975c7767cfc6d3f54bc5e2c9b23fd7db5ad2c57
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-30 21:36:00 +0000

    Added detail to get_config method.

--- pyQCD/core/lattice.py
+++ pyQCD/core/lattice.py
@@ -1,12 +1,14 @@
 from kernel import lattice
 import wilslps, propagator, config
 import dicts
+import itertools
+import numpy as np
 
 class Lattice(lattice.Lattice):
     
-    def __init__(self, L = 4, T = 8, beta = 5.5, u0 = 1.0, action = "wilson",
-                 n_cor = 10, update_method = "heatbath", parallel_updates = True,
-                 block_size = 4, rand_seed = -1):
+    def __init__(self, L=4, T=8, beta=5.5, u0=1.0, action="wilson",
+                 n_cor=10, update_method="heatbath", parallel_updates=True,
+                 block_size=4, rand_seed=-1):
         
         self.beta = beta
         self.u0 = u0
@@ -23,7 +25,22 @@ class Lattice(lattice.Lattice):
                                  rand_seed)
     
     def get_config(self):
-        out = config.Config()
+        
+        raw_links = []
+        r = xrange(self.L)
+        t = xrange(self.T)
+        sites = itertools.product(t, r, r, r, range(4))
+        
+        links = np.zeros((self.T, self.L, self.L, self.L, 4, 3, 3),
+                         dtype=complex)
+        
+        for t, x, y, z, mu in sites:
+            links[t][x][y][z][mu] = np.array(self.get_link([t, x, y, z, mu]))
+            
+        out = config.Config(links, self.L, self.T, self.beta, self.u0,
+                            self.action, self.n_cor, self.update_method,
+                            self.parallel_updates, self.block_size,
+                            self.rand_seed)
         
         return out
     
