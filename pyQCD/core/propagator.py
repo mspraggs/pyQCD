267bed2407ae51a741e412f99c4808c3955b2bb1
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-31 10:31:32 +0000

    Fleshed out propagator constructor and repr function.

--- pyQCD/core/propagator.py
+++ pyQCD/core/propagator.py
@@ -1,11 +1,95 @@
+import numpy as np
 
 class Propagator:
     
-    def __init__(self):
-        pass
+    def __init__(self, propagator, L, T, beta, u0, action, mass, source_site,
+                 num_field_smears, field_smearing_param, num_source_smears,
+                 source_smearing_param, num_sink_smears, sink_smearing_param):
+        """Create a propagator container.
+                 
+        :param propagator: The propagator array
+        :type links: :class:`np.ndarray` with shape
+        :samp:`(T, L, L, L, 4, 4, 3, 3)`
+        :param L: The spatial extent of the corresponding :class:`Lattice`
+        :type L: :class:`int`
+        :param T: The temporal extent of the corresponding :class:`Lattice`
+        :type T: :class:`int`
+        :param beta: The inverse coupling
+        :type beta: :class:`float`
+        :param u0: The mean link
+        :type u0: :class:`float`
+        :param action: The gauge action
+        :type action: :class:`str`, one of wilson, rectangle_improved or
+        twisted_rectangle_improved
+        :param mass: The bare quark mass
+        :type mass: :class:`float`
+        :param source_site: The source site used when doing the inversion
+        :type source_site: :class:`list`
+        :param num_field_smears: The number of stout smears applied when
+        computing the propagator
+        :type num_field_smears: :class:`int`
+        :param field_smearing_param: The stout smearing parameter
+        :type field_smearing_param: :class:`float`
+        :param num_source_smears: The number of Jacobian smears performed
+        on the source when computing the propagator
+        :type num_source_smears: :class:`int`
+        :param source_smearing_param: The Jacobi smearing parameter used
+        when smearing the source
+        :type source_smearing_param: :class:`float`
+        :param num_sink_smears: The number of Jacobian smears performed
+        on the source when computing the propagator
+        :type num_sink_smears: :class:`int`
+        :param sink_smearing_param: The Jacobi smearing parameter used
+        when smearing the source
+        :type sink_smearing_param: :class:`float`
+        :raises: ValueError
+        """
+        
+        expected_shape = (T, L, L, L, 4, 4, 3, 3)
+        
+        if propagator.shape != expected_shape:
+            raise ValueError("Shape of specified propagator array, {}, does not "
+                             "match the specified lattice extents, {}"
+                             .format(propagator.shape, expected_shape))
+        
+        self.L = L
+        self.T = T
+        self.beta = beta
+        self.u0 = u0
+        self.action = action
+        self.mass = mass
+        self.source_site = source_site
+        self.num_field_smears = num_field_smears
+        self.field_smearing_param = field_smearing_param
+        self.num_source_smears = num_source_smears
+        self.source_smearing_param = source_smearing_param
+        self.num_sink_smears = sink_smearing_param
+        self.sink_smearing_param = sink_smearing_param
+        
+        self.data = links
     
-    def __mul__(self):
-        pass
-    
-    def __str__(self):
-        pass
+    def __repr__(self):
+        
+        out = \
+          "Field Configuration Object\n" \
+        "--------------\n" \
+        "Spatial extent: {}\n" \
+        "Temportal extent: {}\n" \
+        "Gauge action: {}\n" \
+        "Inverse coupling (beta): {}\n" \
+        "Mean link (u0): {}\n" \
+        "Bare quark mass (m): {}\n" \
+        "Inversion source site: {}\n" \
+        "Number of stout field smears: {}\n" \
+        "Stout smearing parameter: {}\n" \
+        "Number of source jacobi smears: {}\n" \
+        "Source Jacobi smearing parameter: {}\n" \
+        "Number of sink jacobi smears: {}\n" \
+        "Sink Jacobi smearing parameter: {}\n" \
+        .format(self.L, self.T, self.action, self.beta,
+                self.u0, self.mass = mass, self.source_site,
+                self.num_field_smears, self.field_smearing_param,
+                self.num_source_smears, self.source_smearing_param,
+                self.num_sink_smears, self.sink_smearing_param)
+        
+        return out
