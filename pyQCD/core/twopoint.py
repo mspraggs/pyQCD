7cf7365e11922f79359d1926bedcb65c9c8a5b60
Matt Spraggs <matthew.spraggs@gmail.com>
2013-10-31 20:20:02 +0000

    Starting add constructor for two-point function.

--- pyQCD/core/twopoint.py
+++ pyQCD/core/twopoint.py
@@ -1,11 +1,8 @@
+from observable import Observable
 
-class TwoPoint:
+class TwoPoint(Observable):
     
     def __init__(self, prop1, prop2):
-        pass
-    
-    def save(self, filename):
-        pass
-    
-    def pion_correlator(self):
-        pass
+        """Create a two-point function from two propagators
+        """
+        
