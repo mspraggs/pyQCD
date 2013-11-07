919b5103677a8af943313880a14375ff734479e6
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-07 17:28:52 +0000

    Added correlator calculation and energy extraction functions to TwoPoint.

--- pyQCD/core/twopoint.py
+++ pyQCD/core/twopoint.py
@@ -2,6 +2,8 @@ from observable import Observable
 from propagator import Propagator
 import numpy as np
 import constants as const
+import itertools
+import scipy.optimize as spop
 
 class TwoPoint(Observable):
     
@@ -28,7 +30,9 @@ class TwoPoint(Observable):
         
         self.prop1 = prop1
         self.prop2 = prop2
-        self.computed_correlators = {}
+        self.L = prop1.L
+        self.T = prop1.T
+        self.computed_correlators = []
 
     
     def save(self, filename):
@@ -129,24 +133,130 @@ class TwoPoint(Observable):
         
         return zip(const.Gamma_mesons, const.Gamma_combinations)
     
-    def meson_correlator(self, meson, momentum = [0, 0, 0],
+    def meson_correlator(self, mesons, momenta = [0, 0, 0],
                          average_momenta = True):
         """Computes and stores the specified meson correlator within the
         current TwoPoint object
         
-        :param meson: The meson interpolater to use in calculating the
+        :param meson: The meson interpolater(s) to use in calculating the
         correlator
-        :type meson: :class:`str`, possibilities given by available_mesons
-        :param momentum: The lattice momentum to project the correlator onto
-        :type momentum: :class:`list` of three :class:`int`s, or a compound
+        :type meson: :class:`str` or :class:`str` of strings, possibilities
+        given by available_mesons
+        :param momenta: The lattice momentum to project the correlator onto
+        :type momenta: :class:`list` of three :class:`int`s, or a compound
         list containing several momenta
         :param average_momenta: Determines whether equivalent momenta are
         averaged over
         :type average_momenta: :class:`bool`
         """
+        if type(mesons) == str:
+            mesons = [mesons]
         
-        Gamma = const.Gammas[meson]
+        # First compile a list of all momenta we need to compute for
+        all_momenta = []
+        if type(momenta[0]) != list:
+            momenta = [momenta]
+            
+        for p in momenta:
+            if average_momenta:
+                equiv_momenta = TwoPoint._get_all_momenta(p)
+                equiv_momenta = [[2 * np.pi / self.L * n for n in p]
+                                 for p in equiv_momenta]
+                all_momenta.append(equiv_momenta)
+            else:
+                all_momenta.append([[2 * np.pi / self.L * n for n in p]])
+                
+        # We'll need this for calculating the exponentials
+        sites = list(itertools.product(xrange(self.L),
+                                       xrange(self.L),
+                                       xrange(self.L)))
         
+        # Now go through all momenta and all mesons and compute the correlators
+        for meson in mesons:
+            Gamma = const.Gammas[meson]
+            position_correlator = self._compute_correlator(Gamma)
+            position_correlator = np.reshape(position_correlator,
+                                             (self.prop1.T, self.L**3))
+            for i, ps in enumerate(all_momenta):
+                correlator_sum = np.zeros(self.prop1.T)
+                
+                for p in ps:
+                    exponential_prefactors = np.exp(1j * np.dot(sites, p))
+                    correlator_sum += np.dot(position_correlator,
+                                             exponential_prefactors).real
+                    
+                correlator_sum /= len(ps)
+                
+                member_name = "{}_px{}_py{}_pz{}".format(meson, *momenta[i])
+                if not member_name in self.computed_correlators:
+                    self.computed_correlators.append(member_name)
+                
+                setattr(self, member_name, correlator_sum)
+                
+    def compute_energy(self, particles, fit_range, momenta = [0, 0, 0],
+                       average_momenta = True):
+        """Computes the energy of the specified particles at the specified
+        momenta
+        
+        :param particles: The particle or list of particle to find the energy
+        of
+        :type particles: :class:`str` or :class:`list`
+        :param fit_range: The two time slices specifying the range to fit
+        over
+        :type fit_range: :class:`list` of two :class:`int`s
+        :param momenta: The momentum or a list of momenta to compute the
+        energy for
+        :type momenta: :class:`list`
+        :param average_momenta: Determines whether equivalent momenta should
+        be averaged over
+        :type average_momenta: :class:`bool`
+        """
+        
+        # First make sure that all relevant correlators have been calculated
+        if type(momenta[0]) != list:
+            momenta = [momenta]
+            
+        if type(particles) != list:
+            particles = [particles]
+            
+        outstanding_particles = []
+        outstanding_momenta = []
+            
+        for particle in particles:
+            for momentum in momenta:
+                if not hasattr(self, "{}_px{}_py{}_pz{}".format(particle,
+                                                                *momentum)):
+                    outstanding_particles.append(particle)
+                    outstanding_momenta.append(momentum)
+        
+        self.meson_correlator(outstanding_particles, outstanding_momenta,
+                              average_momenta)
+        
+        energies = []
+        keys = []
+
+        for particle in particles:
+            for momentum in momenta:
+                attrib_name = "{}_px{}_py{}_pz{}".format(particle, *momentum)
+                current_correlator \
+                  = getattr(self, attrib_name)
+                x = np.arange(current_correlator.size)[fit_range[0]:fit_range[1]]
+                y = current_correlator[fit_range[0]:fit_range[1]]
+                
+                fit_function = lambda b, t, Ct: \
+                  Ct - b[0] * (np.exp(-b[1] * (self.T - t)) + np.exp(-b[1] * t))
+                  
+                b, result = spop.leastsq(fit_function, [1.0, 1.0], args=(x, y))
+                
+                if [1, 2, 3, 4].count(result) < 1:
+                    print("Warning: fit failed for {} with momentum {}"
+                          .format(particle, momentum))
+                    
+                energies.append(b[1])
+                keys.append(attrib_name)
+                
+        return dict(zip(keys, energies))
+                        
     def __repr__(self):
         
         out = \
@@ -185,4 +295,59 @@ class TwoPoint(Observable):
                 self.prop2.num_source_smears, self.prop2.source_smearing_param,
                 self.prop2.num_sink_smears, self.prop2.sink_smearing_param)
         
+        return out
+
+    def _compute_correlator(self, gamma):
+        """Calculates the correlator for all space-time points
+        
+        We're doing the following (g = Gamma, p1 = self.prop1,
+        p2 = self.prop2, g5 = const.gamma5):
+        
+        sum_{spin,colour} g * g5 * p1 * g5 * g * p2
+        
+        The g5s are used to find the Hermitian conjugate of the first
+        propagator
+        """
+        
+        Gamma1 = np.matrix(np.dot(gamma, const.gamma5))
+        Gamma2 = np.matrix(np.dot(const.gamma5, gamma))
+        
+        gp1 = Gamma1 * self.prop1.conjugate()
+        gp2 = Gamma2 * self.prop2
+        """
+        gp1 = np.tensordot(Gamma1, np.conj(self.prop1), (1, 1))
+        gp1 = np.swapaxes(gp1, 0, 1)
+        
+        gp2 = np.tensordot(Gamma2, self.prop2, (1, 2))
+        gp2 = np.swapaxes(gp2, 0, 1)
+        gp2 = np.swapaxes(gp2, 1, 2)
+        """
+        return np.einsum('txyzijab,txyzijab->txyz',
+                         gp1.data, gp2.data).real
+
+    @staticmethod
+    def _get_all_momenta(p):
+        """Generates all possible equivalent lattice momenta
+        
+        :param p: The lattice momentum to find equivalent momenta of
+        :type p: :class:`list` with three elements
+        :returns: :class:`list` containing the equivalent momenta
+        """
+        temp_list = []
+        out = []
+        p_rev = p[::-1]
+        
+        for i in xrange(3):
+            temp_list.append([p[(j + i) % 3] for j in xrange(3)])
+            temp_list.append([-p[(j + i) % 3] for j in xrange(3)])
+            temp_list.append([p_rev[(j + i) % 3] for j in xrange(3)])
+            temp_list.append([-p_rev[(j + i) % 3] for j in xrange(3)])
+            
+        # Remove duplicates
+        for p2 in temp_list:
+            if not p2 in out:
+                out.append(p2)
+            
+        out.sort()
+            
         return out
