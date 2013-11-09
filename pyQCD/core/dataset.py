5bb012eaaef3df7d4a3398afe35dbd2091b7b287
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-09 00:06:01 +0000

    Added constructor and add datum functions to data set.

--- pyQCD/core/dataset.py
+++ pyQCD/core/dataset.py
@@ -1,12 +1,63 @@
+import cPickle
+import zipfile
+import os
 
 class DataSet:
     # Generic data set class. Contains header that denotes type of data.
     # Routines to perform statistical measurements on the entire dataset
-    def __init__(self):
-        pass
+    def __init__(self, datatype, filename, compress=True):
+        """Create an empty data set holding data of the specified type
+        
+        :param datatype: The data type stored in the data set
+        :type datatype: :class:`type`
+        :param filename: The npz file to save the data to
+        :type filename: :class:`str`
+        """
+        
+        storage_mode = zipfile.ZIP_DEFLATED if compress else zipfile.ZIP_STORED
+        
+        self.datatype = datatype
+        self.num_data = 0
+        self.filename = filename
+        
+        try:
+            zfile = zipfile.ZipFile(filename, 'w', storage_mode, True)
+            self.large_file = True
+        except RuntimeError:
+            print("Warning: > 2GB data set not supported.")
+            storage_mode = zipfile.ZIP_STORED
+            zfile = zipfile.ZipFile(filename, 'w', storage_mode, False)
+            self.large_file = False
+            
+        self.storage_mode = storage_mode
+        
+        typefile = open("type", 'w')
+        cPickle.dump(datatype, typefile)
+        typefile.close()
+        
+        zfile.write("type")
+        zfile.close()
+        os.unlink("type")
     
     def add_datum(self, datum):
-        pass
+        """Adds a datum to the dataset
+        
+        :param datum: The data to add
+        :type datum: The type specified in the object constructor
+        """
+        
+        if type(datum) != self.datatype:
+            raise TypeError("Supplied data type {} does not match the required "
+                            "data type {}".format(type(datum), self.datatype))
+        
+        datum.save("{}{}".format(self.datatype.__name__, self.num_data))
+        
+        with zipfile.ZipFile(self.filename, 'a', self.storage_mode,
+                             self.large_file) as zfile:
+            zfile.write("{}{}.npz".format(self.datatype.__name__, self.num_data))
+        
+        os.unlink("{}{}.npz".format(self.datatype.__name__, self.num_data))
+        self.num_data += 1
     
     def get_datum(self, index):
         pass
@@ -16,6 +67,7 @@ class DataSet:
     
     @classmethod
     def load(self, filename):
+        pass
 
     def compute_observable(self, measurement, num_bootstraps, jacknife,
                            bin_size, args):
