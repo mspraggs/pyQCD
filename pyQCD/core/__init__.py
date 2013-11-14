f943c00200b4cf69091cf035d3042e15e629a8f8
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-14 22:43:02 +0000

    Added automatic imports of types in __init__ files

---
+++ pyQCD/core/__init__.py
@@ -0,0 +1,7 @@
+from lattice import Lattice
+from observable import Observable
+from propagator import Propagator
+from config import Config
+from twopoint import TwoPoint
+from wilslps import WilsonLoops
+from dataset import DataSet
