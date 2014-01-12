Fixed speed of light calculation in test.

pyQCD/test/test_pyQCD.py
@@ -1132,7 +1132,7 @@ class TestTwoPoint:
         
         expected_result \
           = np.array([(energy1**2 - mass**2) / (np.pi / 4),
-                      (energy2**2 - mass**2) / (np.pi / 4 * np.sqrt(2))])
+                      (energy2**2 - mass**2) / (np.pi / 2)])
         
         for m, p in zip([mass, energy1, energy2], momenta):
             correlator = amplitude * np.exp(-m * np.arange(twopoint.T)) \