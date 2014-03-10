5df011730652bf68d2337ec212728ab72a587c60
Matt Spraggs <matthew.spraggs@gmail.com>
2014-03-10 19:30:21 +0000

    Set up CMAKE and paths in preparation for linear operator implementation.

---
+++ pyQCD/core/kernel/include/fermion_actions/linear_operator.hpp
@@ -0,0 +1,24 @@
+#ifdef LINEAR_OPERATOR_HPP
+#define LINEAR_OPERATOR_HPP
+
+#include <complex>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include <omp.h>
+
+#include <lattice.hpp>
+
+using namespace Eigen;
+using namespace std;
+
+class LinearOperator
+{
+public:
+  LinearOperator();
+  
+  VectorXcd apply(const VectorXcd& x);
+  VectorXcd apply_hermitian(const VectorXcd& x);
+  VectorXcd undo_hermiticity(const VectorXcd& x);
+}
