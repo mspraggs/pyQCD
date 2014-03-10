4cc8cc76fbd655b90f581e3374443e659feaf03f
Matt Spraggs <matthew.spraggs@gmail.com>
2014-03-10 21:52:04 +0000

    Fleshed out linear operator include file.

--- pyQCD/core/kernel/include/fermion_actions/linear_operator.hpp
+++ pyQCD/core/kernel/include/fermion_actions/linear_operator.hpp
@@ -1,24 +1,31 @@
 #ifndef LINEAR_OPERATOR_HPP
 #define LINEAR_OPERATOR_HPP
 
-#include <complex>
-#include <vector>
-
 #include <Eigen/Dense>
 
 #include <omp.h>
-
-#include <lattice.hpp>
 
 using namespace Eigen;
 using namespace std;
 
 class LinearOperator
 {
+  // The base class from which all Dirac operators are derived
 public:
-  LinearOperator();
+  LinearOperator() { };
+  ~LinearOperator() { };
   
-  VectorXcd apply(const VectorXcd& x);
-  VectorXcd apply_hermitian(const VectorXcd& x);
-  VectorXcd undo_hermiticity(const VectorXcd& x);
-}
+  // Applies the linear operator to a column vector using right multplication
+  virtual VectorXcd apply(const VectorXcd& x)
+  { return VectorXcd::Zero(x.size()); }
+  // Applies a hermition form of the the linear operator to a column vector
+  // using right multiplication
+  virtual VectorXcd applyHermitian(const VectorXcd& x)
+  { return VectorXcd::Zero(x.size()); }
+  // Undoes the hermiticity operation applied by applyHermitian. Note that
+  // undoHermiticity(applyHermitian(x)) = apply(x)
+  virtual VectorXcd undoHermiticity(const VectorXcd& x)
+  { return VectorXcd::Zero(x.size()); }
+};
+
+#endif
