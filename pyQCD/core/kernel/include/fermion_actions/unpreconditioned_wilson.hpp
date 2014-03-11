baec288207216fdecf499793a17c8d70becda0f5
Matt Spraggs <matthew.spraggs@gmail.com>
2014-03-11 11:03:53 +0000

    Fixed some layout and naming conventions in UnpreconditionedWilson header

--- pyQCD/core/kernel/include/fermion_actions/unpreconditioned_wilson.hpp
+++ pyQCD/core/kernel/include/fermion_actions/unpreconditioned_wilson.hpp
@@ -3,11 +3,13 @@
 
 #include <Eigen/Dense>
 
+#include <complex>
+
 #include <omp.h>
 
 #include <lattice.hpp>
 #include <utils.hpp>
-#include "linear_operator.hpp"
+#include <fermion_actions/linear_operator.hpp>
 
 using namespace Eigen;
 using namespace std;
@@ -17,15 +19,16 @@ class UnpreconditionedWilson : public LinearOperator
   // Basic unpreconditioned Wilson Dirac operator
 
 public:
-  UnpreconditionedWilson(const double mass, const Lattice* lattice);
+  UnpreconditionedWilson(const double mass, Lattice* lattice);
   ~UnpreconditionedWilson();
 
-  VectorXcd apply(const VectorXcd& x);
-  VectorXcd applyHermitian(const VectorXcd& x);
-  VectorXcd undoHermiticity(const VectorXcd& x);
+  VectorXcd apply(const VectorXcd& psi);
+  VectorXcd applyHermitian(const VectorXcd& psi);
+  VectorXcd undoHermiticity(const VectorXcd& psi);
 
 private:
-  Lattice* lattice_; // Pointer to the lattice object containing the gauge links
+  // Pointer to the lattice object containing the gauge links
+  Lattice* lattice_;
   double mass_; // Mass of the Wilson fermion
   int operatorSize_; // Size of vectors on which the operator may operate
   // The 1 +/- gamma_mu matrices required by the operator
@@ -33,3 +36,5 @@ private:
   // The spinStructes_ above multiplied by gamma_5 from the right
   vector<Matrix4cd, aligned_allocator<Matrix4cd> > hermitianSpinStructures_;
 };
+
+#endif
