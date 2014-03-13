c71690ce7ebfaae31e5d18dbfa12cd3f5697f90c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-03-13 20:02:55 +0000

    Fixed more naming conventions in JacobiSmearing.

--- pyQCD/core/kernel/src/linear_operators/jacobi_smearing.cpp
+++ pyQCD/core/kernel/src/linear_operators/jacobi_smearing.cpp
@@ -28,14 +28,14 @@ JacobiSmearing::JacobiSmearing(
 								       0.0));
 
     // Determine the coordinates of the site we're on
-    int site[5]; // The coordinates of the lattice site
+    int site[5]; // The coordinates of the lattice site/link
     pyQCD::getLinkIndices(4 * i, this->lattice_->spatialExtent,
 			  this->lattice_->temporalExtent, site);
 
     int siteBehind[5]; // Site/link index for the site/link behind us
     int siteAhead[5]; // Site/link index for the site/link in front of us
 
-    // Loop over the four gamma indices (j) in the sum inside the Wilson action
+    // Loop over the three gamma indices (j) in the sum inside the Wilson action
     for (int j = 0; j < 4; ++j) {
           
       // Now we determine the indices of the neighbouring links
@@ -95,7 +95,8 @@ VectorXcd JacobiSmearing::apply(const VectorXcd& psi)
 
 VectorXcd JacobiSmearing::applyOnce(const VectorXcd& psi)
 {
-  // Right jltiply a vector once by the operator
+  // Right multiply a vector once by the operator H (see Jacobi smearing in
+  // Gattringer and Lang).
   VectorXcd eta = VectorXcd::Zero(this->operatorSize_); // The output vector
 
   // If psi's the wrong size, get out of here before we segfault
@@ -104,7 +105,7 @@ VectorXcd JacobiSmearing::applyOnce(const VectorXcd& psi)
 
 #pragma omp parallel for
   for (int i = 0; i < this->operatorSize_; ++i) {
-    int etaSiteIndex = i / 12; // Site index of the current row in 
+    int etaSiteIndex = i / 12; // Site index of the current row in eta
     int alpha = (i % 12) / 3; // Spin index of the current row in eta
     int a = i % 3; // Colour index of the current row in eta
 
@@ -114,27 +115,27 @@ VectorXcd JacobiSmearing::applyOnce(const VectorXcd& psi)
       
       // Now we determine the indices of the neighbouring links
 
-      int x_minus_j_index = this->nearestNeighbours_[etaSiteIndex][j];
-      int x_plus_j_index = this->nearestNeighbours_[etaSiteIndex][j + 4];
+      int siteBehindIndex = this->nearestNeighbours_[etaSiteIndex][j];
+      int siteAheadIndex = this->nearestNeighbours_[etaSiteIndex][j + 4];
 
       // Now loop over spin and colour indices for the portion of the row we're
       // on
 
-      for (int j = 0; j < 12; ++j) {
-	int beta = j / 3; // Compute spin
-	int b = j % 3; // Compute colour
+      for (int k = 0; k < 12; ++k) {
+	int beta = k / 3; // Compute spin
+	int b = k % 3; // Compute colour
 	eta(i)
 	  -= 0.5 * this->identity_(alpha, beta)
 	  * this->boundaryConditions_[etaSiteIndex][j]
-	  * conj(this->lattice_->getLink(4 * x_minus_j_index + j)(b, a))
-	  * psi(12 * x_minus_j_index + 3 * beta + b)
+	  * conj(this->lattice_->getLink(4 * siteBehindIndex + j)(b, a))
+	  * psi(12 * siteBehindIndex + 3 * beta + b)
 	  / this->lattice_->u0();
 	
 	eta(i)
 	  -= 0.5 * this->identity_(alpha, beta)
 	  * this->boundaryConditions_[etaSiteIndex][j + 4]
 	  * this->lattice_->getLink(4 * etaSiteIndex + j)(a, b)
-	  * psi(12 * x_plus_j_index + 3 * beta + b)
+	  * psi(12 * siteAheadIndex + 3 * beta + b)
 	  / this->lattice_->u0();
       }
     }
