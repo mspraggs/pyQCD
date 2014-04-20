a802d8239a78918353ff36d00afc94c83bc7b926
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-20 21:50:50 +0100

    Added back and forward substitution functions to linear operator class

--- pyQCD/core/kernel/include/linear_operators/linear_operator.hpp
+++ pyQCD/core/kernel/include/linear_operators/linear_operator.hpp
@@ -42,10 +42,8 @@ public:
   { return complex<double>(0.0, 0.0); }
   virtual VectorXcd applyDiagonal(const VectorXcd& x)
   { return VectorXcd::Zero(x.size()); }
-  virtual VectorXcd forwardSubstitute(const VectorXcd& x)
-  { return VectorXcd::Zero(x.size()); }
-  virtual VectorXcd backSubstitute(const VectorXcd& x)
-  { return VectorXcd::Zero(x.size()); }
+  VectorXcd forwardSubstitute(const VectorXcd& b)
+  VectorXcd backSubstitute(const VectorXcd& b);
   VectorXcd applySsor(const VectorXcd& x)
   {
     VectorXcd v = this->backSubstitute(x);
---
+++ pyQCD/core/kernel/src/linear_operators/linear_operator.cpp
@@ -0,0 +1,45 @@
+#include <linear_operators/linear_operator.hpp>
+
+VectorXcd LinearOperator::forwardSubstitute(const VectorXcd& b)
+{
+  // Solve L*x = b using forward substitution
+
+  VectorXcd x = VectorXcd::Zero(this->operatorSize_);
+
+  if (b.size() != this->operatorSize_)
+    return x;
+
+  // First get the diagonal elements
+  VectorXcd diagonal = this->applyDiagonal(VectorXcd::One(this->operatorSize_));
+
+  for (int i = 0; i < this->operatorSize_; ++i) {
+    x[i] = b[i];
+    x[i] -= this->lowerRowDot(x, i);
+    x[i] /= diagonal(i);
+  }
+
+  return x;
+}
+
+
+
+VectorXcd LinearOperator::backSubstitute(const VectorXcd& b)
+{
+  // Solve L*x = b using forward substitution
+
+  VectorXcd x = VectorXcd::Zero(this->operatorSize_);
+
+  if (b.size() != this->operatorSize_)
+    return x;
+
+  // First get the diagonal elements
+  VectorXcd diagonal = this->applyDiagonal(VectorXcd::One(this->operatorSize_));
+
+  for (int i = this->operatorSize_ - 1; i >= 0; --i) {
+    x[i] = b[i];
+    x[i] -= this->upperRowDot(x, i);
+    x[i] /= diagonal(i);
+  }
+
+  return x;
+}
