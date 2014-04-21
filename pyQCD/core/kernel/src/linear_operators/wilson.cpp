1384d3620bb9c8c48c7a67d86d4ffd8c5be972d8
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-21 16:32:52 +0100

    Fixed incorrect coefficients in Wilson lower and upper rowDots

--- pyQCD/core/kernel/src/linear_operators/wilson.cpp
+++ pyQCD/core/kernel/src/linear_operators/wilson.cpp
@@ -68,14 +68,14 @@ VectorXcd Wilson::makeHermitian(const VectorXcd& psi)
 
 complex<double> Wilson::lowerRowDot(const VectorXcd& psi, const int row)
 {
-  return -this->hoppingMatrix_->lowerRowDot(psi, row);
+  return -0.5 * this->hoppingMatrix_->lowerRowDot(psi, row);
 }
 
 
 
 complex<double> Wilson::upperRowDot(const VectorXcd& psi, const int row)
 {
-  return -this->hoppingMatrix_->upperRowDot(psi, row);
+  return -0.5 * this->hoppingMatrix_->upperRowDot(psi, row);
 }
 
 
