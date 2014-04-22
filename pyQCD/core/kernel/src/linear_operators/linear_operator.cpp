5a54307e695754ce1b5897e6e64d09991e2ca5dd
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-22 15:58:29 +0100

    Fixed incorrect variable reference in LinearOperator::removeEvenOdd

--- pyQCD/core/kernel/src/linear_operators/linear_operator.cpp
+++ pyQCD/core/kernel/src/linear_operators/linear_operator.cpp
@@ -47,7 +47,7 @@ VectorXcd LinearOperator::removeEvenOdd(const VectorXcd& x)
 #pragma omp parallel for
   for (int i = nSites / 2; i < nSites; ++i)
     for (int j = 0; j < 12; ++j)
-      y(12 * this->oddIndices_[i - numSites / 2] + j) = x(12 * i + j);
+      y(12 * this->oddIndices_[i - nSites / 2] + j) = x(12 * i + j);
 
   return y;
 }
