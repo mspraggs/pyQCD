a9cde460a055742efec7af7a84a869da1b8e4069
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-28 21:59:13 +0100

    Added 3d hopping kernel to cuda kernels.

--- pyQCD/core/kernel/cuda/kernels.h
+++ pyQCD/core/kernel/cuda/kernels.h
@@ -23,6 +23,13 @@ void hoppingKernel(Complex* y, const Complex* x, const Complex* links,
 		   const Complex* boundaryConditions, const Complex scaling,
 		   const int L, const int T);
 
+template<int numHops>
+__global__ 
+void hoppingKernel3d(Complex* y, const Complex* x, const Complex* links,
+		     const Complex* gammas, const int* neighbourIndices,
+		     const Complex* boundaryConditions, const Complex scaling,
+		     const int L, const int T);
+
 #include <kernels.tcu>
 
 #endif
