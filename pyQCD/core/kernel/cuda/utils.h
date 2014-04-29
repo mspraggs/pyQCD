c85fdc57342cce67693ad97edfcdab7161017dae
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-29 13:56:12 +0100

    Added propagator types to utils.h

--- pyQCD/core/kernel/cuda/utils.h
+++ pyQCD/core/kernel/cuda/utils.h
@@ -8,6 +8,8 @@ typedef cusp::complex<float> Complex;
 typedef cusp::linear_operator<cusp::complex<float>, cusp::device_memory> super;
 typedef cusp::array1d<Complex, cusp::device_memory> VectorTypeDev;
 typedef cusp::array1d<Complex, cusp::host_memory> VectorTypeHost;
+typedef cusp::array2d<Complex, cusp::device_memory> PropagatorTypeDev;
+typedef cusp::array2d<Complex, cusp::host_memory> PropagatorTypeHost;
 
 void createGammas(Complex* gammas);
 void diagonalSpinMatrices(Complex* matrices, Complex factor);
