cc03b71c7d36c1c29993a1b5f898b77718251adc
Matt Spraggs <matthew.spraggs@gmail.com>
2014-04-30 14:06:07 +0100

    Added missing jacobi smearing cuda files

---
+++ pyQCD/core/kernel/cuda/jacobi_smearing.cu
@@ -0,0 +1,105 @@
+#include <wilson.h>
+
+JacobiSmearing::JacobiSmearing(const int nSmears, const double smearingParameter,
+			       const int L, const int T,
+			       const Complex boundaryConditions[4],
+			       Complex* links, const bool copyLinks)
+  : LinearOperator(L, T, false, false, links, copyLinks)
+{
+  this->nSmears_ = nSmears;
+  this->smearingParameter_ = smearingParameter;
+
+  // First generate the neighbours
+  int numSites = this->N / 12;
+  int size = 8 * numSites * sizeof(int);
+  int* hostIndices = (int*) malloc(size);
+  cudaMalloc((void**) &this->neighbourIndices_, size);
+  generateNeighbours(hostIndices, 1, L, T);
+  cudaMemcpy(this->neighbourIndices_, hostIndices, size,
+	     cudaMemcpyHostToDevice);
+  free(hostIndices);
+
+  // Now generate the spin structures needed. There are eight, one for each
+  // of the hops that have to be performed. The array has to be flattened, as
+  // CUDA doesn't like 2D arrays
+  size = 8 * 16 * sizeof(Complex); // Eight matrices, 16 complex numbers each
+  Complex hostGammas[64]; // We'll store temporary gamma matrices here
+  Complex hostSpinStructures[128]; // We'll set up the spin structures here
+  // before sending them to the device
+  createGammas(hostGammas); // Initialise the gamma matrices
+
+  // Now set the spin structes up with the identity, which we'll then subtract
+  // and add the gamma matrices from/to.
+  diagonalSpinMatrices(hostSpinStructures, Complex(1.0, 0.0));
+  diagonalSpinMatrices(hostSpinStructures + 64, Complex(1.0, 0.0));
+  // Now send everything to the device
+  cudaMalloc((void**) &this->spinStructures_, size);
+  cudaMemcpy(this->spinStructures_, hostSpinStructures, size,
+	     cudaMemcpyHostToDevice);
+
+  // Set up the boundary conditions for one hop
+  size = 8 * numSites * sizeof(Complex);
+  Complex* hostBoundaryConditions = (Complex*) malloc(size);
+  generateBoundaryConditions(hostBoundaryConditions, 1,
+			     boundaryConditions, L, T);
+  cudaMalloc((void**) &this->boundaryConditions_, size);
+  cudaMemcpy(this->boundaryConditions_, hostBoundaryConditions, size,
+	     cudaMemcpyHostToDevice);
+  free(hostBoundaryConditions);
+}
+
+
+
+JacobiSmearing::~JacobiSmearing()
+{
+  cudaFree(this->neighbourIndices_);
+  cudaFree(this->spinStructures_);
+  cudaFree(this->boundaryConditions_);
+}
+
+
+
+void JacobiSmearing::applyOnce(Complex* y, const Complex* x) const
+{  
+  int dimBlock;
+  int dimGrid;
+
+  setGridAndBlockSize(dimBlock, dimGrid, this->N);
+
+  diagonalKernel<<<dimGrid,dimBlock>>>(y, x, 0.0,
+				       this->L_, this->T_);
+  
+  hoppingKernel3d<1><<<dimGrid,dimBlock>>>(y, x, this->links_, 
+					   this->spinStructures_,
+					   this->neighbourIndices_,
+					   this->boundaryConditions_,
+					   this->smearingParameter_,
+					   this->L_, this->T_);
+}
+
+
+
+void JacobiSmearing::apply(Complex* y, const Complex* x) const
+{
+  int dimBlock;
+  int dimGrid;
+
+  setGridAndBlockSize(dimBlock, dimGrid, this->N);
+
+  Complex* z;
+  cudaMalloc((void**) &z, this->N * sizeof(Complex));
+  assignDev<<<dimGrid,dimBlock>>>(z, x, this->N);
+
+  Complex* w;
+  cudaMalloc((void**) &w, this->N * sizeof(Complex));
+  assignDev<<<dimGrid,dimBlock>>>(w, 0.0, this->N);
+
+  for (int i = 0; i < this->nSmears_; ++i) {
+    this->applyOnce(w, z)
+    saxpyDev<<<dimGrid,dimBlock>>>(y, w, 1.0, this->L_, this->T_);
+    assingDev<<<dimGrid,dimBlock>>>(z, w)
+  }
+
+  cudaFree(z);
+  cudaFree(w);
+}
---
+++ pyQCD/core/kernel/cuda/jacobi_smearing.h
@@ -0,0 +1,31 @@
+#ifndef CUDA_JACOBI_SMEARING_H
+#define CUDA_JACOBI_SMEARING_H
+
+#include <linear_operator.h>
+
+#include <utils.h>
+#include <kernels.h>
+
+class JacobiSmearing : public LinearOperator
+{
+public:
+  JacobiSmearing(const int nSmears, const double smearingParameter,
+		 const int L, const int T, const bool precondition,
+		 const Complex* boundaryConditions, Complex* links,
+		 const bool copyLinks);
+  ~JacobiSmearing();
+
+  void applyOnce(Complex* y, const Complex* x) const;
+  void apply(Complex* y, const Complex* x) const;
+
+private:
+  int nSmears_;
+  double smearingParameter_;
+
+  int* neighbourIndices_;
+
+  Complex* spinStructures_;
+  Complex* boundaryConditions_;
+};
+
+#endif
