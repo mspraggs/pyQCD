94046934ec4c29bc3ab56407a83cbcdd82c93f1c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-01 16:11:08 +0100

    Added external definition of invertWilsonDiracOperator to utils.hpp

--- pyQCD/core/kernel/include/utils.hpp
+++ pyQCD/core/kernel/include/utils.hpp
@@ -110,6 +110,14 @@ namespace pyQCD
 
 #ifdef USE_CUDA
   void cudaFormatGaugeField(Complex* cuspField, const GaugeField& eigenField);
+  
+  extern void invertWilsonDiracOperator(VectorTypeHost& psi, const VectorTypeHost& eta,
+					const double mass,
+					const Complex boundaryConditions[4],
+					const int solverMethod, const int precondition,
+					const int maxIterations, const double tolerance,
+					const int verbosity, const Complex* gaugeField,
+					const int L, const int T);
 #endif
 
 }
