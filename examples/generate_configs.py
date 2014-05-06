46528dcf01d12658237395e9ce94b8c70b3ab8c6
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-06 23:27:11 +0100

    Updated correlator and config examples to reflect changes in code

--- examples/generate_configs.py
+++ examples/generate_configs.py
@@ -19,23 +19,21 @@ if __name__ == "__main__":
     # Now we can specify the measurements we want to do. All we want to do here
     # is store the field configurations, so we use the function
     # pyQCD.Lattice.get_config to do this, which is a member function of the
-    # lattice object. This function returns the type Config, so we specify the
-    # type as such here too. The resulting configs will be stored in pyQCD's
-    # DataSet type, before being written to disk, so we need to specify a
-    # file name for the output. It is also possible to specify a message that
-    # is outputted when the measurement takes place. Here we specify one.
-    simulation.add_measurement(pyQCD.Lattice.get_config, pyQCD.Config,
-                               "4c8_ensemble.zip",
+    # lattice object. The resulting configs will be stored in a zip archive,
+    # so we need to specify a file name for the output. It is also possible to
+    # specify a message that is outputted when the measurement takes place.
+    # Here we specify one.
+    simulation.add_measurement(pyQCD.Lattice.get_config, "4c8_ensemble.zip",
                                meas_message="Saving current config")
     
     # Now that the lattice and measurements have been set up, the simulation can
     # be run
     simulation.run()
 
-    # Once finished, there should be a zip archive called 4c8_ensemble.zip in the
-    # examples directory, which should contain 100 configurations.
+    # Once finished, there should be a zip archive called 4c8_ensemble.zip in
+    # the examples directory, which should contain 100 configurations.
     # In addition, since the default options were used when calling run(), the
     # simulation object would, at the end of the simulation, contain a member
-    # variable called plaquettes, containing the mean plaquette value for each of
-    # the configurations. This could optionally be saved to disk as a numpy
+    # variable called plaquettes, containing the mean plaquette value for each
+    # of the configurations. This could optionally be saved to disk as a numpy
     # binary.
