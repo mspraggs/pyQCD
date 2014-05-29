f75cdeccd834fd15b7c2625a3d6665aaf235318d
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-29 21:35:33 +0100

    Added extensible compute_propagator function to propagator.py

--- pyQCD/core/propagator.py
+++ pyQCD/core/propagator.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import logging
+
 import numpy as np
 
 from .constants import gamma5
@@ -79,3 +81,103 @@ def spin_prod(a, b):
         out = np.transpose(out, (1, 2, 3, 4, 0, 5, 6, 7))
         
         return out
+
+def compute_propagator(src_func, invert_func, src_smear=None, snk_smear=None):
+    """Extensible propagator generation function
+
+    Args:
+      src_func (function): A function used to generate the source used by the
+        inversion function. This function must accept exactly two arguments.
+        The first should be the spin index of the source and the second the
+        colour index of the source. The resulting source should have shape
+        (T, L, L, L, 4, 3).
+      invert_func (function): The function used to perform the inversion. This
+        function must accept exactly one argument - the source of shape
+        (T, L, L, L, 4, 3). The function should return either the solution or
+        a tuple containing the solution, the number of iterations performed by
+        the solver, the final residual and the CPU time.
+      src_smear (function, optional): The smearing function to apply to the
+        source before inverting. This should accept one argument only, the
+        source to smear, and return the smeared source. Both variables should
+        have shape (T, L, L, L, 4, 3).
+      snk_smear (function, optional): The smearing function to apply to the
+        sink after inverting. This should accept one argument only, the sink
+        to smear, and return the smeared source. Both variables should have
+        shape (T, L, L, L, 4, 3).
+
+    Returns:
+      numpy.ndarray: The propagator as a numpy ndarray
+
+    Examples:
+      Create a lattice, then compute the Wilson propagator at tree level. Since
+      the smearing functions are ommitted, no smearing is performed.
+
+      >>> import pyQCD
+      >>> lattice = pyQCD.Lattice(4, 8, 5.5, "wilson", 10)
+      >>> make_source = lambda s, c: lattice.point_source(s, c, [0, 0, 0, 0])
+      >>> inverter = lambda eta: lattice.invert_wilson_dirac(eta, 0.4)
+      >>> prop = pyQCD.compute_propagator(make_source, inverter)
+
+      Here we do some smearing too:
+
+      >>> import pyQCD
+      >>> lattice = pyQCD.Lattice(4, 8, 5.5, "wilson", 10)
+      >>> make_source = lambda s, c: pyQCD.point_source(s, c, [0, 0, 0, 0])
+      >>> smear_func = lambda psi: pyQCD.apply_jacobi_smearing(psi, 2, 1.5)
+      >>> inverter = lambda eta: lattice.invert_wilson_dirac(eta, 0.4)
+      >>> prop = pyQCD.compute_propagator(make_source, inverter,
+      ...                                 smear_func, smear_func)
+    """
+
+    prop_log = logging.getLogger("propagator")
+    invert_log = logging.getLogger("propagator.invert")
+
+    spinor_shape = src_func(0, 0).shape
+
+    propagator = np.zeros((4, 3) + spinor_shape, dtype=np.complex)
+
+    prop_log.info("Started computing propagator")
+    
+    for spin in range(4):
+        for colour in range(3):
+            prop_log.info("Inverting for spin {} and colour {}"
+                         .format(spin, colour))
+            
+            prop_log.info("Generating source")
+            source = src_func(spin, colour)
+            
+            if src_smear != None:
+                prop_log.info("Smearing source")
+                source = src_smear(source)
+
+            invert_log.info("Now inverting...")
+            result = invert_func(source)
+            if type(result) == tuple:
+                solution = result[0]
+                invert_log.info("Inversion finished after {} iterations "
+                                "with residual {}".format(result[1], result[2]))
+                invert_log.info("CPU time for this inversion: {} seconds"
+                                .format(result[3]))
+            else:
+                solution = result
+                invert_log.info("Done!")
+
+            if snk_smear != None:
+                prop_log.info("Smearing sink")
+                solution = snk_smear(solution)
+
+            propagator[spin, colour] = solution
+
+    prop_log.info("Finished computing propagator")
+
+    # This is a bit ugly, but I can't see another way to do this for an
+    # arbitrary shaped propagator (e.g. 5D).
+
+    shape_size = len(propagator.shape)
+    # We're looking to construct the permuted ordering of the propagator
+    # indices so we can put the spin and colour indices at the end. E.g.:
+    # (4, 3, T, L, L, L, 4, 3) -> (T, L, L, L, 4, 4, 3, 3)
+    transposed_order = (tuple(range(2, shape_size - 2))
+                        + (shape_size - 2, 0, shape_size - 1, 1))
+
+    return np.transpose(propagator, transposed_order)
