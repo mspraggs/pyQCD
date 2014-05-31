d272bd767cdc771c552ac477f7cafd15d1672bcd
Matt Spraggs <matthew.spraggs@gmail.com>
2014-05-31 18:17:43 +0100

    Added decorators for logging function arguments.

--- pyQCD/core/log.py
+++ pyQCD/core/log.py
@@ -8,8 +8,112 @@ import logging
 import inspect
 
 def _logger():
+    
     stack = inspect.stack()
     for frame in stack:
         if not frame[3].startswith("_"):
+            obj = frame[0], frame[3]
             break
-    return logging.getLogger(inspect.getmodule(frame[0]).__name__)
+    
+    names = [inspect.getmodule(obj[0]).__name__]
+    
+    try:
+        names.append(obj[0].f_locals['self'].__class__.__name__)
+    except KeyError:
+        pass
+
+    names.append(obj[1])
+    
+    return logging.getLogger(".".join(names))
+
+def make_func_logger(f, args):
+
+    # First construct a logger based on module, class and function names
+    names = [inspect.getmodule(f).__name__]
+    if inspect.isclass(args[0].__class__):
+        names.append(args[0].__class__.__name__)
+    names.append(f.__name__)
+    return logging.getLogger(".".join(names))
+
+def merge_defaults(argspec, kwargs):
+
+    if argspec.defaults == None:
+        return kwargs
+    else:
+        kwargs_defaults = dict(zip(argspec.args[-len(argspec.defaults):],
+                                   argspec.defaults))
+        kwargs_defaults.update(kwargs)
+
+        return kwargs_defaults
+
+class Log(object):
+
+    def __init__(self, message=None, ignore=()):
+        self.init_message = message
+        self.ignore=ignore + ("self",)
+
+    def __call__(self, f):
+
+        def _wrapper(*args, **kwargs):
+
+            logger = make_func_logger(f, args)
+
+            if self.init_message != None:
+                logger.info(self.init_message)
+            
+            argspec = inspect.getargspec(f)
+            # List the arguments that don't have defaults
+            self.print_args(zip(argspec.args, args), logger)
+
+            # Now merge any default values with the kwargs and list these
+            kwargs = merge_defaults(argspec, kwargs)
+            self.print_args(kwargs.items(), logger)
+
+            return f(*args, **kwargs)
+
+        return _wrapper
+
+    def print_args(self, args, logger):
+
+        for key, val in args:
+            if key in self.ignore:
+                continue
+            logger.info("{}: {}".format(key, val))
+        
+class ApplyLog(Log):
+
+    def __init__(self, operator_label):
+        self.init_message = "Applying {} operator...".format(operator_label)
+        self.ignore = ("self", "psi")
+
+class InversionLog(Log):
+
+    def __init__(self, action_label):
+        self.init_message = ("Inverting {} Dirac operator..."
+                             .format(action_label))
+        self.ignore = ("self", "eta", "solver_info")
+
+    def __call__(self, f):
+
+        def _wrapper(*args, **kwargs):
+
+            logger = make_func_logger(f, args)
+            argspec = inspect.getargspec(f)
+
+            # List the arguments that don't have defaults
+            self.print_args(zip(argspec.args, args), logger)
+
+            # Now merge any default values with the kwargs and list these
+            kwargs = merge_defaults(argspec, kwargs)
+            kwargs.update({"solver_info": True})
+            self.print_args(kwargs.items(), logger)
+
+            result = f(*args, **kwargs)
+
+            logger.info("Solver finished after {} iterations with residual {}"
+                        .format(result[1], result[2]))
+            logger.info("CPU time used: {}".format(result[3]))
+
+            return result[0]
+
+        return _wrapper
