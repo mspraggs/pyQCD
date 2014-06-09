01b9df9dedd9b96bebf4d1bc10640f23add86d6d
Matt Spraggs <matthew.spraggs@gmail.com>
2014-06-09 17:44:47 +0100

    Added convenience function to generator submodule
    This takes the code produced by the Generator object and injects it into
    a linear operator template.

--- pyQCD/codegen/generator.py
+++ pyQCD/codegen/generator.py
@@ -3,11 +3,72 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import os
 import ast
 import inspect
 import re
 import random
 import string
+
+import jinja2
+
+template_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            'templates')
+
+def generate(input_file, header_file="template.hpp",
+             source_file="template.cpp"):
+    header_file = os.path.join(template_dir, header_file)
+    source_file = os.path.join(template_dir, source_file)
+
+    with open(header_file) as f:
+        header_template = jinja2.Template(f.read())
+    with open(source_file) as f:
+        source_template = jinja2.Template(f.read())
+
+    with open(input_file) as f:
+        tree = ast.parse(f.read())
+    generator = Generator(tree)
+
+    dest_stem = (os.path.split(input_file)[1][:-3]
+                 if input_file.endswith(".py")
+                 else os.path.split(input_file)[1])
+
+    template_args = {}
+    template_args.update(ctor_body=generator.funcgen("__init__"))
+    template_args.update(apply_body=generator.funcgen("apply"))
+    template_args.update(apply_herm_body=generator.funcgen("apply_hermitian"))
+    template_args.update(make_herm_body=generator.funcgen("make_hermitian"))
+    template_args.update(apply_even_even_inv_body
+                         =generator.funcgen("apply_even_even_inv"))
+    template_args.update(apply_odd_odd_body=generator.funcgen("apply_odd_odd"))
+    template_args.update(apply_even_odd_body
+                         =generator.funcgen("apply_even_odd"))
+    template_args.update(apply_odd_even_body
+                         =generator.funcgen("apply_odd_even"))
+    template_args.update(destructibles=generator.destructibles())
+
+    if "HoppingTerm*" in generator.member_types.values():
+        hopper = [key for key, value in generator.member_types.items()
+                  if value == "HoppingTerm*"][0]
+        even_odd_handling \
+          = ("this->evenIndices_ = this->{}->getEvenIndices();\n"
+             "this->oddIndices_ = this->{}->getOddIndices();"
+             .format(hopper, hopper))
+        even_odd_handling = generator.format(even_odd_handling, 1)
+    else:
+        even_odd_handling = ""
+
+    template_args.update(ctor_args=generator.declare_args('__init__'))
+    template_args.update(member_vars=generator.declare_members())
+    template_args.update(class_name=generator.classname)
+    template_args.update(include_guard=dest_stem.upper())
+    template_args.update(include_name=dest_stem)
+    template_args.update(even_odd_handling=even_odd_handling)
+
+    with open("{}.hpp".format(dest_stem), 'w') as f:
+        f.write(header_template.render(**template_args))
+    with open("{}.cpp".format(dest_stem), 'w') as f:
+        f.write(source_template.render(**template_args))
 
 class Generator(object):
 
