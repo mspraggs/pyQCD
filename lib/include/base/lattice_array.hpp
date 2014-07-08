cb7b6ac97d9fe47f2690ce1a9aac4277519389ce
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-08 17:15:37 +0100

    Fleshed out the declaration of the LatticeArray template

--- lib/include/base/lattice_array.hpp
+++ lib/include/base/lattice_array.hpp
@@ -1,3 +1,6 @@
+#ifndef LATTICE_ARRAY_HPP
+#define LATTICE_ARRAY_HPP
+
 /* This file provides a container for lattice-wide objects. This class serves as
  * a base for all other lattice objects, e.g. LatticeGaugeField, LatticeSpinor,
  * etc.
@@ -11,16 +14,43 @@
  * arithmetic operations involving LatticeArray.
  */
 
-template <typename T>
-class LatticeArray
+#include <vector>
+
+#include <utils/macros.hpp>
+
+namespace pyQCD
 {
+  template <typename T>
+  class LatticeArray
+  {
 
-public:
-  LatticeArray();
+  public:
+    // Constructors
+    LatticeArray();
+    LatticeArray(const vector<int>& lattice_shape);
+    LatticeArray(const LatticeArray<T>& lattice_array);
+    virtual ~LatticeArray();
 
-protected:
-  vector<vector<T> > data_;
+    // Shared constructor code
+    void init(const vector<int>& lattice_shape);
 
-private:
-  vector<vector<int> > layout_;
-};
+    // 
+    LatticeArray<T>& operator=(const LatticeArray<T>& rhs);
+    const T& operator[](COORD_INDEX_ARGS(n)) const;
+    T& operator[](COORD_INDEX_ARGS(n));
+
+    T& datum_ref(const int index);
+    const &T datum_ref(const int index) const;
+    T& datum_ref(const int i, const int j);
+    const &T datum_ref(const int i, const int j) const;
+
+  protected:
+    vector<vector<T> > _data;
+
+  private:
+    vector<int> _shape;
+    vector<vector<int> > _layout;
+  };
+}
+
+#endif
