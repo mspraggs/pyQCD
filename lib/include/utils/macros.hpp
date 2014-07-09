9d39fd4e90272421f06ad486634752fced3bf007
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-09 10:17:06 +0100

    Added macro to specify list of coordinate index parameters.

--- lib/include/utils/macros.hpp
+++ lib/include/utils/macros.hpp
@@ -6,6 +6,7 @@
  * dimensions and colours in the simulation.
  */
 
+// Macro to specify coordinate arguments of a function
 #if NDIM==1
 #define COORD_INDEX_ARGS(n) const int n ## 0
 #endif
@@ -46,6 +47,42 @@
 #define COORD_INDEX_ARGS(n) const int n ## 0, const int n ## 1, \
     const int n ## 2, const int n ## 3, const int n ## 4, const int n ## 5, \
     const int n ## 6, const int n ## 7, const int n ## 8, const int n ## 9
+#endif
+
+// Macro to specify coordinate parameters to a function
+#if NDIM==1
+#define COORD_INDEX_PARAMS(n) n ## 0
+#endif
+#if NDIM==2
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1
+#endif
+#if NDIM==3
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2
+#endif
+#if NDIM==4
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3
+#endif
+#if NDIM==5
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4
+#endif
+#if NDIM==6
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4, n ## 5
+#endif
+#if NDIM==7
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4, n ## 5, \
+    n ## 6
+#endif
+#if NDIM==8
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4, n ## 5, \
+    n ## 6, n ## 7
+#endif
+#if NDIM==9
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4, n ## 5, \
+    n ## 6, n ## 7, n ## 8
+#endif
+#if NDIM==10
+#define COORD_INDEX_PARAMS(n) n ## 0, n ## 1, n ## 2, n ## 3, n ## 4, n ## 5, \
+    n ## 6, n ## 7, n ## 8, n ## 9
 #endif
 
 #endif
