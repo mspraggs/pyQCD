115a7d846e0cdc142253779c99d13c0cc67b756c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-09 10:17:53 +0100

    Changed datum_ref function that accepts one argument to operator[] funciton.

--- lib/include/base/lattice_array.hpp
+++ lib/include/base/lattice_array.hpp
@@ -43,8 +43,8 @@ namespace pyQCD
     T& operator()(COORD_INDEX_ARGS(n));
 
     // Functions to access the _data member directly
-    T& datum_ref(const int index);
-    const T& datum_ref(const int index) const;
+    T& operator[](const int index);
+    const T& operator[](const int index) const;
     T& datum_ref(const int i, const int j);
     const T& datum_ref(const int i, const int j) const;
 
