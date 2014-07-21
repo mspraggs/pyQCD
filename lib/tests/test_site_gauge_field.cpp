a56a7459db3f8108a05c4a11bcda4c5d6644666b
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-21 23:42:44 +0100

    Fleshed out constructor tests for SiteGaugeField
    Added FieldExposed struct to allow direct access to SiteGaugeField data
    Added constructor test function
    Added function to test all values in data member are equal to supplied
    value
    Refactored code for readability and consistency
    Added param_test_case for checking values are equal to a supplied value

--- lib/tests/test_site_gauge_field.cpp
+++ lib/tests/test_site_gauge_field.cpp
@@ -12,28 +12,61 @@
 typedef Eigen::Matrix3cd ColourType
 typedef pyQCD::SiteGaugeField<ColourType> Field;
 
-void constructor_test(const Field& gauge_field)
+MatrixCompare<ColourType> matrix_compare(1e-8);
+
+
+
+struct FieldExposed : public Field
 {
   
 }
+
+
+
+void constructor_test(const FieldExposed& gauge_field)
+{
+  BOOST_REQUIRE_EQUAL(gauge_field.data().size(), NDIM)
+}
+
+
+
+void const_value_test(const FieldExposed& gauge_field,
+		      const ColourType& value)
+{
+  bool all_vals_equal = true;
+  for (int i = 0; i < NDIM; ++i)
+    if (not matrix_compare(gauge_field.data()[i], value)) {
+      BOOST_TEST_MESSAGE("All elements in SiteGaugeField not equal to supplied "
+			 "value");
+      all_vals_equal = false;
+      break;
+    }
+  BOOST_CHECK(all_vals_equal);
+}
+
+
 
 BOOST_AUTO_TEST_SUITE(test_site_gauge_field)
 
 BOOST_AUTO_TEST_CASE(test_constructors)
 {
   TestRandom rng;
-  MatrixCompare<ColourType> matrix_compare(1e-8);
-  Field::colour_type rand_col_mat = Field::colour_type::Random();
+  ColourType rand_col_mat = ColourType::Random();
 
-  std::vector<Field> gauge_fields;
-  gauge_fields.push_back(Field());
-  gauge_fields.push_back(Field(rand_col_mat));
-  gauge_fields.push_back(Field(gauge_fields[1]));
-  Field test_equals = gauge_fields[1];
+  std::vector<FieldExposed> gauge_fields;
+  gauge_fields.push_back(FieldExposed());
+  gauge_fields.push_back(FieldExposed(rand_col_mat));
+  gauge_fields.push_back(FieldExposed(gauge_fields[1]));
+  FieldExposed test_equals = gauge_fields[1];
   gauge_fields.push_back(test_equals);
   
   BOOST_PARAM_TEST_CASE(constructor_test,
 			gauge_fields.begin(), gauge_fields.end());
+
+  boost::unit_test::callback1<BaseDouble> bound_const_value_test
+    = boost::bind(&const_value_test, _1, rand_col_mat);
+  BOOST_PARAM_TEST_CASE(bound_const_value_test,
+			gauge_fields.begin() + 1, gauge_fields.end());
 }
 
 BOOST_AUTO_TEST_CASE(test_utils)
