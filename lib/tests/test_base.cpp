a944630263ff8e7fb49cfd552600502d923d67f3
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-21 12:20:15 +0100

    Refactored test_base.cpp to reduce code repetition.

--- lib/tests/test_base.cpp
+++ lib/tests/test_base.cpp
@@ -1,11 +1,36 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE base_test
-#include <boost/test/unit_test.hpp>
-
+#define BOOST_TEST_MODULE test_base
 #include <vector>
 #include <exception>
+#include <random>
+#include <functional>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/parameterized_test.hpp>
+#include <boost/bind.hpp>
 
 #include <base/lattice_base.hpp>
+
+typedef pyQCD::LatticeBase<double> Base;
+
+struct TestRandom
+{
+  TestRandom()
+  {
+    BOOST_TEST_MESSAGE("Set up random number generator");
+    gen = std::mt19937(rd());
+    real_dist = std::uniform_real_distribution<>(0, 10);
+    int_dist = std::uniform_int_distribution<>(0, 100);
+  }
+  ~TestRandom() { BOOST_TEST_MESSAGE("Tear down random number generator"); }
+  int gen_int() { return int_dist(gen); }
+  double gen_real() { return real_dist(gen); }
+
+  std::random_device rd;
+  std::mt19937 gen;
+  std::uniform_real_distribution<> real_dist;
+  std::uniform_int_distribution<> int_dist;
+};
 
 struct TestLayout
 {
@@ -26,10 +51,7 @@ struct TestLayout
       num_blocks = lattice_volume / block_volume;
     }
   }
-  ~TestLayout()
-  { 
-    BOOST_TEST_MESSAGE("Tear down test layout");
-  };
+  ~TestLayout() { BOOST_TEST_MESSAGE("Tear down test layout"); }
 
   std::vector<int> lattice_shape;
   std::vector<int> bad_latt_shape;
@@ -39,45 +61,62 @@ struct TestLayout
   int num_blocks;
 };
 
+
+
+void constructor_test(const pyQCD::LatticeBase<double>& lattice_base,
+		      const TestLayout& layout)
+{
+  BOOST_CHECK_EQUAL_COLLECTIONS(lattice_base.lattice_shape().begin(),
+				lattice_base.lattice_shape().end(),
+				layout.lattice_shape.begin(),
+				layout.lattice_shape.end());
+  BOOST_CHECK_EQUAL_COLLECTIONS(lattice_base.block_shape().begin(),
+				lattice_base.block_shape().end(),
+				layout.block_shape.begin(),
+				layout.block_shape.end());
+  BOOST_CHECK_EQUAL(lattice_base.layout().size(), layout.lattice_volume);
+  BOOST_CHECK_EQUAL(lattice_base.lattice_volume(), layout.lattice_volume);
+  BOOST_CHECK_EQUAL(lattice_base.block_volume(), layout.block_volume);
+  BOOST_CHECK_EQUAL(lattice_base.num_blocks(), layout.num_blocks);
+}
+
+
+
+void const_value_test(const pyQCD::LatticeBase<double>& lattice_base,
+		      const double value)
+{
+  for (int i = 0; i < lattice_base.lattice_volume(); ++i)
+    BOOST_CHECK_CLOSE(lattice_base[i], value, 1e-5);
+}
+
+
+
 BOOST_AUTO_TEST_SUITE(test_lattice_base)
 
-BOOST_AUTO_TEST_CASE(constructors)
+BOOST_AUTO_TEST_CASE(test_constructors)
 {
-  typedef pyQCD::LatticeBase<double> Base;
   TestLayout layout;
-  Base test_latt_base(layout.lattice_shape, layout.block_shape);
-  BOOST_CHECK_EQUAL_COLLECTIONS(test_latt_base.lattice_shape().begin(),
-				test_latt_base.lattice_shape().end(),
-				layout.lattice_shape.begin(),
-				layout.lattice_shape.end());
-  BOOST_CHECK_EQUAL_COLLECTIONS(test_latt_base.block_shape().begin(),
-				test_latt_base.block_shape().end(),
-				layout.block_shape.begin(),
-				layout.block_shape.end());
-  BOOST_CHECK_EQUAL(test_latt_base.layout().size(), layout.lattice_volume);
-  BOOST_CHECK_EQUAL(test_latt_base.lattice_volume(), layout.lattice_volume);
-  BOOST_CHECK_EQUAL(test_latt_base.block_volume(), layout.block_volume);
-  BOOST_CHECK_EQUAL(test_latt_base.num_blocks(), layout.num_blocks);
+  TestRandom rng;
+  double rand_num = rng.gen_real();
+  Base test_const_init(rand_num, layout.lattice_shape, layout.block_shape);
 
-  BOOST_CHECK_THROW(Base test_latt_base_bad(layout.bad_latt_shape,
-					    layout.block_shape),
-		    std::invalid_argument);
+  std::vector<Base> test_bases;
+  test_bases.push_back(Base(layout.lattice_shape, layout.block_shape));
+  test_bases.push_back(test_const_init);
+  test_bases.push_back(Base(test_const_init));
 
-  Base test_init_const(10.0, layout.lattice_shape, layout.block_shape);
-  BOOST_CHECK_EQUAL_COLLECTIONS(test_init_const.lattice_shape().begin(),
-				test_init_const.lattice_shape().end(),
-				layout.lattice_shape.begin(),
-				layout.lattice_shape.end());
-  BOOST_CHECK_EQUAL_COLLECTIONS(test_init_const.block_shape().begin(),
-				test_init_const.block_shape().end(),
-				layout.block_shape.begin(),
-				layout.block_shape.end());
-  BOOST_CHECK_EQUAL(test_init_const.layout().size(), layout.lattice_volume);
-  BOOST_CHECK_EQUAL(test_init_const.lattice_volume(), layout.lattice_volume);
-  BOOST_CHECK_EQUAL(test_init_const.block_volume(), layout.block_volume);
-  BOOST_CHECK_EQUAL(test_init_const.num_blocks(), layout.num_blocks);
-  for (int i = 0; i < test_init_const.lattice_volume(); ++i)
-    BOOST_CHECK_CLOSE(test_init_const[i], 10.0, 1e-5);
+  boost::unit_test::callback1<Base> bound_constructor_test
+    = boost::bind(&constructor_test, _1, layout);
+  BOOST_PARAM_TEST_CASE(bound_constructor_test,
+			test_bases.begin(), test_bases.end());
+
+  BOOST_CHECK_THROW(Base bad_base(layout.bad_latt_shape, layout.block_shape),
+		    std::invalid_argument)
+
+  BOOST_TEST_CASE(boost::bind(const_value_test,
+			      test_bases[1], rand_num));
+  BOOST_TEST_CASE(boost::bind(const_value_test,
+			      test_bases[2], rand_num));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
