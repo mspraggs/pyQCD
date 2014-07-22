354cf6ba28a38bc80d3bb832578375fe3b7e6d23
Matt Spraggs <matthew.spraggs@gmail.com>
2014-07-22 17:48:10 +0100

    Fixed more indentation in LatticeBase.

--- lib/include/base/lattice_base.hpp
+++ lib/include/base/lattice_base.hpp
@@ -157,8 +157,8 @@ namespace pyQCD
   
   template <typename T, int ndim>
   LatticeBase<T, ndim>::LatticeBase(const T& init_value,
-				const std::vector<int>& lattice_shape,
-				const std::vector<int>& block_shape)
+				    const std::vector<int>& lattice_shape,
+				    const std::vector<int>& block_shape)
     : _lattice_shape(lattice_shape), _block_shape(block_shape)
   {
     // Constructor for given lattice size etc -> values in _data initialized
