8230167a219f65e744f928b8c371405ca12dbb21
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-22 15:16:42 +0100

    Cannot overload class templates - classes in templates.hpp now have separate names

--- lib/include/utils/templates.hpp
+++ lib/include/utils/templates.hpp
@@ -5,56 +5,61 @@
  * including extending the type_traits utilities where necessary.
  */
 
-// The following structs allow us to determine whether a given class is derived
-// from a template base class. I've adapted this from the example by bluescarni
-// from http://stackoverflow.com/questions/5997956 \
-// /how-to-determine-if-a-type-is-derived-from-a-template-class
+/* The following structs allow us to determine whether a given class is derived
+ * from a template base class. I've adapted this from the example by bluescarni
+ * from http://stackoverflow.com/questions/5997956		\
+ * /how-to-determine-if-a-type-is-derived-from-a-template-class
+ */
 
-template <template <typename...> class F>
-struct conversion_tester
+#include <type_traits>
+
+namespace pyQCD
 {
-  template <typename... Args>
-  conversion_tester (const F<Args...>&);
-};
-
-template <class From, template <typename...> class To>
-struct is_instance_of
-{
-  static const bool value
-  = std::is_convertible<From, conversion_tester<To> >::value;
-};
-
-
-
-template <template <int...> class F>
-struct conversion_tester
-{
-  template <int... Args>
-  conversion_tester (const F<Args...>&);
-};
-
-template <class From, template <int...> class To>
-struct is_instance_of
-{
-  static const bool value
-  = std::is_convertible<From, conversion_tester<To> >::value;
-};
+  template <template <typename...> class F>
+  struct conversion_tester_type_temp
+  {
+    template <typename... Args>
+    conversion_tester_type_temp (const F<Args...>&);
+  };
+  
+  template <class From, template <typename...> class To>
+  struct is_instance_of_type_temp
+  {
+    static const bool value
+    = std::is_convertible<From, conversion_tester_type_temp<To> >::value;
+  };
 
 
 
-// Overloaded for cases like LatticeBase<T, ndim>
-template <template <typename, int> class F>
-struct conversion_tester
-{
-  template <typename T, int N>
-  conversion_tester (const F<T, N>&);
-};
+  template <template <int, int...> class F>
+  struct conversion_tester_int_temp
+  {
+    template <int N, int... Args>
+    conversion_tester_int_temp (const F<N, Args...>&);
+  };
 
-template <class From, template <typename, int> class To>
-struct is_instance_of
-{
-  static const bool value
-  = std::is_convertible<From, conversion_tester<To> >::value;
-};
+  template <class From, template <int, int...> class To>
+  struct is_instance_of_int_temp
+  {
+    static const bool value
+    = std::is_convertible<From, conversion_tester_int_temp<To> >::value;
+  };
+  
+  
+  
+  template <template <typename, int> class F>
+  struct conversion_tester_LatticeBase
+  {
+    template <typename T, int N>
+    conversion_tester_LatticeBase (const F<T, N>&);
+  };
+  
+  template <class From, template <typename, int> class To>
+  struct is_instance_of_LatticeBase
+  {
+    static const bool value
+    = std::is_convertible<From, conversion_tester_LatticeBase<To> >::value;
+  };
+}
 
 #endif
