491a8bafb95410e09773ebf8f8a1f710c4dc421b
Matt Spraggs <matthew.spraggs@gmail.com>
2015-01-27 22:49:01 +0000

    Added TestRandom class to helpers.hpp

--- lib/tests/helpers.hpp
+++ lib/tests/helpers.hpp
@@ -54,4 +54,24 @@ struct MatrixCompare
   T absolute_tolerance_;
 };
 
+
+struct TestRandom
+{
+  TestRandom()
+  {
+    INFO("Set up random number generator");
+    gen = std::mt19937(rd());
+    real_dist = std::uniform_real_distribution<>(0, 10);
+    int_dist = std::uniform_int_distribution<>(0, 100);
+  }
+  ~TestRandom() { INFO("Tear down random number generator"); }
+  int gen_int() { return int_dist(gen); }
+  double gen_real() { return real_dist(gen); }
+
+  std::random_device rd;
+  std::mt19937 gen;
+  std::uniform_real_distribution<> real_dist;
+  std::uniform_int_distribution<> int_dist;
+};
+
 #endif
