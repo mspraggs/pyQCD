ee573a61f3eb848ac6e7e323e6330128ac4f0eda
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-11 01:33:44 +0000

    Added layout test

---
+++ lib/tests/test_layout.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+
+#include <base/layout.hpp>
+
+#include "helpers.hpp"
+
+
+TEST_CASE("LexicoLayout test") {
+  pyQCD::LexicoLayout layout(std::vector<unsigned int>{8, 4, 4, 4});
+
+  for (int i = 0; i < 512; ++i) {
+    REQUIRE (layout.get_array_index(i) == i);
+    REQUIRE (layout.get_site_index(i) == i);
+  }
+  REQUIRE (layout.get_array_index(std::vector<unsigned int>{4, 3, 2, 1})
+             == 313);
+}
