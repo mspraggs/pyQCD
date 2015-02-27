8ef49428b3f3bf0013b21a5f6ec7d0366a2c22fc
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-27 14:08:58 +0000

    Added test_lattice file

---
+++ lib/tests/test_lattice.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include <base/lattice.hpp>
+
+#include "helpers.hpp"
+#include "../base/layout.hpp"
+
+typedef pyQCD::Lattice<double> Lattice;
+
+TEST_CASE("Lattice test") {
+  pyQCD::LexicoLayout(std::vector<unsigned int>{8, 4, 4, 4});
+}
