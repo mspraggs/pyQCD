9c641d6f34c745172e8cb154c105335531fdada5
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-27 19:07:11 +0000

    Added arithmetic test for testing expression templates in test_lattice.cpp

--- lib/tests/test_lattice.cpp
+++ lib/tests/test_lattice.cpp
@@ -7,5 +7,15 @@
 typedef pyQCD::Lattice<double> Lattice;
 
 TEST_CASE("Lattice test") {
-  pyQCD::LexicoLayout(std::vector<unsigned int>{8, 4, 4, 4});
+  pyQCD::LexicoLayout layout(std::vector<unsigned int>{8, 4, 4, 4});
+
+  Lattice lattice1(&layout, 1.0);
+  Lattice lattice2(&layout, 2.0);
+
+  SECTION("Test arithmetic operators") {
+    Lattice lattice3 = lattice1 + lattice2;
+    for (auto val : lattice3) {
+      REQUIRE(val == 3.0);
+    }
+  }
 }
