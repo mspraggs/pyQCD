bdf22540982833700c6217a19998b67a8516ed5b
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-28 15:11:57 +0000

    Added test for layout check in Lattice arithmetic

--- lib/tests/test_lattice.cpp
+++ lib/tests/test_lattice.cpp
@@ -6,16 +6,27 @@
 
 typedef pyQCD::Lattice<double> Lattice;
 
+class TestLayout : public pyQCD::Layout
+{
+public:
+  TestLayout(const std::vector<unsigned int>& shape)
+    : pyQCD::Layout(shape, [] (const unsigned int i) { return i; })
+  { }
+};
+
 TEST_CASE("Lattice test") {
   pyQCD::LexicoLayout layout(std::vector<unsigned int>{8, 4, 4, 4});
+  TestLayout another_layout(std::vector<unsigned int>{8, 4, 4, 4});
 
   Lattice lattice1(&layout, 1.0);
   Lattice lattice2(&layout, 2.0);
+  Lattice bad_lattice(&another_layout, 2.0);
 
   SECTION("Test arithmetic operators") {
     Lattice lattice3 = lattice1 + lattice2;
     for (auto val : lattice3) {
       REQUIRE(val == 3.0);
     }
+    REQUIRE_THROWS(lattice1 + bad_lattice);
   }
 }
