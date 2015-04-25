5e8cb7091298c49f2947b363a863631f030c8c72
Matt Spraggs <matthew.spraggs@gmail.com>
2015-04-25 18:52:54 +0100

    Added test for GaugeField to test adjoint operator

--- lib/tests/test_gauge_field.cpp
+++ lib/tests/test_gauge_field.cpp
@@ -7,15 +7,20 @@
 
 typedef pyQCD::GaugeField<3> GaugeField;
 
-MatrixCompare<Eigen::Matrix3cd> matrix_compare(1.0e-8, 1.0e-5);
-
 TEST_CASE("GaugeField test")
 {
+  MatrixCompare<Eigen::Matrix3cd> matrix_compare(1.0e-8, 1.0e-5);
+
   GaugeField gauge_field1(4, Eigen::Matrix3cd::Identity());
-  GaugeField gauge_field2;
+  GaugeField gauge_field2(4, Eigen::Matrix3cd::Random());
 
   SECTION("Test expressions") {
     GaugeField gauge_field_adj = gauge_field1.adjoint();
     REQUIRE(matrix_compare(gauge_field_adj[0], Eigen::Matrix3cd::Identity()));
+
+    gauge_field_adj = gauge_field2.adjoint();
+    for (int i = 0; i < 4; ++i) {
+      REQUIRE(matrix_compare(gauge_field2[i], gauge_field_adj[i].adjoint()));
+    }
   }
 }
