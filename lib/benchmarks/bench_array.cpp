4e1e3bfd4454b5598520e984917948087e19490a
Matt Spraggs <matthew.spraggs@gmail.com>
2015-04-29 22:42:39 +0100

    Added extra benchmarks for Array

--- lib/benchmarks/bench_array.cpp
+++ lib/benchmarks/bench_array.cpp
@@ -7,18 +7,29 @@
 #include <base/array.hpp>
 
 
-int main()
+int main(int argc, char* argv[])
 {
-  int n = 1000000;
+  int n = 1000;
   typedef Eigen::Matrix2d arr_type;
   arr_type elem = arr_type::Ones();
   pyQCD::Array<arr_type, Eigen::aligned_allocator> array1(n, elem);
   pyQCD::Array<arr_type, Eigen::aligned_allocator> array2(n, elem);
   pyQCD::Array<arr_type, Eigen::aligned_allocator> array3(n, elem);
 
+  std::cout << "Profiling f(x, y, z) = x + y + z:" << std::endl;
   benchmark([&] () {
     decltype(array1) result = array1 + array2 + array3;
-  }, 16 * n);
+  }, 16 * n, 100000);
+
+  std::cout << "Profiling f(x, y) = a * x + y:" << std::endl;
+  benchmark([&] () {
+    decltype(array1) result = 5.0 * array1 + array2;
+  }, 16 * n, 100000);
+
+  std::cout << "Profiling f(x, y, z) = x * y + z:" << std::endl;
+  benchmark([&] () {
+    decltype(array1) result = array1 * array2 + array3;
+  }, 32 * n, 100000);
 
   return 0;
 }
