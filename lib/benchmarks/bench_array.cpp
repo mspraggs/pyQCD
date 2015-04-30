dae19b7075759bccddcffe7a3985464fdd78af70
Matt Spraggs <matthew.spraggs@gmail.com>
2015-04-30 22:12:18 +0100

    Added some decltypes to bench_array to simplify any changes

--- lib/benchmarks/bench_array.cpp
+++ lib/benchmarks/bench_array.cpp
@@ -13,22 +13,23 @@ int main(int argc, char* argv[])
   typedef Eigen::Matrix2d arr_type;
   arr_type elem = arr_type::Ones();
   pyQCD::Array<arr_type, Eigen::aligned_allocator> array1(n, elem);
-  pyQCD::Array<arr_type, Eigen::aligned_allocator> array2(n, elem);
-  pyQCD::Array<arr_type, Eigen::aligned_allocator> array3(n, elem);
+  decltype(array1) array2(n, elem);
+  decltype(array1) array3(n, elem);
+  decltype(array1) result(n, elem);
 
   std::cout << "Profiling f(x, y, z) = x + y + z:" << std::endl;
   benchmark([&] () {
-    decltype(array1) result = array1 + array2 + array3;
+    result = array1 + array2 + array3;
   }, 16 * n, 100000);
 
   std::cout << "Profiling f(x, y) = a * x + y:" << std::endl;
   benchmark([&] () {
-    decltype(array1) result = 5.0 * array1 + array2;
+    result = 5.0 * array1 + array2;
   }, 16 * n, 100000);
 
   std::cout << "Profiling f(x, y, z) = x * y + z:" << std::endl;
   benchmark([&] () {
-    decltype(array1) result = array1 * array2 + array3;
+    result = array1 * array2 + array3;
   }, 32 * n, 100000);
 
   return 0;
