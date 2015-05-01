8ac8cdf5d284a9602c634381db774c9978dd4ce9
Matt Spraggs <matthew.spraggs@gmail.com>
2015-05-01 17:36:53 +0100

    Encapsulated bench array benchmark to add benchmark for double

--- lib/benchmarks/bench_array.cpp
+++ lib/benchmarks/bench_array.cpp
@@ -7,12 +7,13 @@
 #include <base/array.hpp>
 
 
-int main(int argc, char* argv[])
+template <typename T, template <typename> class Alloc = std::allocator>
+void profile_for_type(const T& elem, const std::string& type,
+  const int add_flops, const int multiply_flops)
 {
+  std::cout << "Profiling for array type " << type << "." << std::endl;
   int n = 1000;
-  typedef Eigen::Matrix2d arr_type;
-  arr_type elem = arr_type::Ones();
-  pyQCD::Array<arr_type, Eigen::aligned_allocator> array1(n, elem);
+  pyQCD::Array<T, Alloc> array1(n, elem);
   decltype(array1) array2(n, elem);
   decltype(array1) array3(n, elem);
   decltype(array1) result(n, elem);
@@ -20,17 +21,25 @@ int main(int argc, char* argv[])
   std::cout << "Profiling f(x, y, z) = x + y + z:" << std::endl;
   benchmark([&] () {
     result = array1 + array2 + array3;
-  }, 16 * n, 100000);
+  }, 2 * add_flops * n, 100000);
 
-  std::cout << "Profiling f(x, y) = a * x + y:" << std::endl;
+  std::cout << "Profiling f(x, y) = 5.0 * x + y:" << std::endl;
   benchmark([&] () {
     result = 5.0 * array1 + array2;
-  }, 16 * n, 100000);
+  }, 2 * add_flops * n, 100000);
 
   std::cout << "Profiling f(x, y, z) = x * y + z:" << std::endl;
   benchmark([&] () {
     result = array1 * array2 + array3;
-  }, 32 * n, 100000);
+  }, (add_flops + multiply_flops) * n, 100000);
+}
 
+
+int main(int argc, char* argv[])
+{
+  profile_for_type(1.0, "double", 2, 2);
+  profile_for_type<Eigen::Matrix2d, Eigen::aligned_allocator>(
+    Eigen::Matrix2d::Random(), "Eigen::Matrix2d", 8, 24
+  );
   return 0;
 }
