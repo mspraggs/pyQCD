b88d4e798d721becff5e0aabeb95fc42c4fc82dd
Matt Spraggs <matthew.spraggs@gmail.com>
2015-05-02 22:36:32 +0100

    Updated setup.py to include lib and templates data and removed extensions

--- setup.py
+++ setup.py
@@ -1,62 +1,54 @@
-from setuptools import setup, Extension, find_packages
-from setuptools.command import build_ext
+from setuptools import setup, find_packages
+from itertools import product
 import os
-import io
 
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
+data_dirs = ["lib", "templates"]
+include_subdirs = ["base",
+                   "base/detail",
+                   "fermion_actions",
+                   "gauge_actions",
+                   "linear_operators",
+                   "utils"]
+suffixes = [".hpp", ".cpp", ".pxd", ".pyx"]
 
-long_description = read('README.md')
+data_paths = ["{}/{}/*{}".format(dr, subdir, suffix)
+              for dr, subdir, suffix in product(data_dirs, include_subdirs,
+                                                suffixes)]
 
 
-class my_build_ext(build_ext.build_ext):
-    def build_extension(self, ext):
-        import shutil
-        import os.path
-        try:
-            import pyQCD.core.kernel.lattice
-            module_file = pyQCD.core.kernel.lattice.__file__
-            shutil.copyfile(module_file,
-                            self.get_ext_fullpath(ext.name))
-        except ImportError:
-            print("Notice: lattice.so binary missing. Installing analysis modules only.")
+with open("README.md") as f:
+    long_description = f.read()
+
 
 # Exclude the lattice.py and simulation.py files if lattice.so
 # hasn't been built.
-
-setup(
-    name='pyQCD',
-    version='',
-    packages=find_packages(exclude=["*test*"]),
-    url='http://github.com/mspraggs/pyqcd/',
-    license='Apache Software License',
-    author='Matt Spraggs',
-    install_requires=['numpy', 'scipy'],
-    author_email='matthew.spraggs@gmail.com',
-    description='pyQCD provides a Python library for running coarse lattice '
-    'QCD simulations on desktop and workstation computers.',
-    long_description=long_description,
-    cmdclass = {'build_ext': my_build_ext},
-    ext_modules = [Extension('pyQCD/core/kernel/lattice',
-                             sources = ['pyQCD/core/kernel/src/wrapper.cpp'])],
-    package_dir={'': '.'},
-    package_data={
-        'pyQCD.codegen': ['templates/*.cpp', 'templates/*.hpp'],
-        },
-    classifiers = [
-        'Programming Language :: Python',
-        'Programming Language :: C++',
-        'Development Status :: 3 - Alpha',
-        'Natural Language :: English',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering :: Physics',
-        ],
-)
+try:
+    os.symlink("../lib", "pyQCD/lib")
+    os.symlink("../templates", "pyQCD/templates")
+    setup(
+        name='pyQCD',
+        version='',
+        packages=find_packages(exclude=["*test*"]),
+        url='http://github.com/mspraggs/pyqcd/',
+        author='Matt Spraggs',
+        author_email='matthew.spraggs@gmail.com',
+        description='pyQCD provides a Python library for running lattice field '
+                    'theory simulations on desktop and workstation computers.',
+        long_description=long_description,
+        package_dir={'': '.'},
+        package_data={'pyQCD': data_paths},
+        classifiers = [
+            'Programming Language :: C++',
+            'Programming Language :: Cython',
+            'Programming Language :: Python',
+            'Development Status :: 3 - Alpha',
+            'Natural Language :: English',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Topic :: Scientific/Engineering :: Physics',
+            ],
+    )
+finally:
+    os.unlink("pyQCD/lib")
+    os.unlink("pyQCD/templates")
