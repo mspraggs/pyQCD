5a4ee185cea5faae56265f6176d7942f8308bf3b
Matt Spraggs <matthew.spraggs@gmail.com>
2015-05-04 23:21:05 +0100

    Moved lib files into pyQCD, and cleared existing pyQCD folder

--- pyQCD/core/kernel/include/linear_operators.hpp
+++
@@ -1,9 +0,0 @@
-
-class Lattice;
-
-#include <linear_operators/linear_operator.hpp>
-#include <linear_operators/wilson.hpp>
-#include <linear_operators/hamber_wu.hpp>
-#include <linear_operators/naik.hpp>
-#include <linear_operators/dwf.hpp>
-#include <linear_operators/jacobi_smearing.hpp>
