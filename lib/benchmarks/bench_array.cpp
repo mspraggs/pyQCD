2d29cca8c57789f1236c5fb04bd3f5ee44364886
Matt Spraggs <matthew.spraggs@gmail.com>
2015-05-04 13:06:11 +0100

    Added more benchmarks to bench_array

--- lib/benchmarks/bench_array.cpp
+++ lib/benchmarks/bench_array.cpp
@@ -12,7 +12,7 @@ void profile_for_type(const T& elem, const std::string& type,
   const int add_flops, const int multiply_flops)
 {
   std::cout << "Profiling for array type " << type << "." << std::endl;
-  int n = 1000;
+  int n = 100;
   pyQCD::Array<T, Alloc> array1(n, elem);
   decltype(array1) array2(n, elem);
   decltype(array1) array3(n, elem);
@@ -21,17 +21,17 @@ void profile_for_type(const T& elem, const std::string& type,
   std::cout << "Profiling f(x, y, z) = x + y + z:" << std::endl;
   benchmark([&] () {
     result = array1 + array2 + array3;
-  }, 2 * add_flops * n, 100000);
+  }, 2 * add_flops * n, 1000000);
 
   std::cout << "Profiling f(x, y) = 5.0 * x + y:" << std::endl;
   benchmark([&] () {
     result = 5.0 * array1 + array2;
-  }, 2 * add_flops * n, 100000);
+  }, 2 * add_flops * n, 1000000);
 
   std::cout << "Profiling f(x, y, z) = x * y + z:" << std::endl;
   benchmark([&] () {
     result = array1 * array2 + array3;
-  }, (add_flops + multiply_flops) * n, 100000);
+  }, (add_flops + multiply_flops) * n, 1000000);
   
   std::cout << std::endl;
 }
@@ -43,7 +43,20 @@ int main(int argc, char* argv[])
   profile_for_type(std::complex<double>(1.0, 0.0), "std::complex<double>",
                    4, 12);
   profile_for_type<Eigen::Matrix2d, Eigen::aligned_allocator>(
-    Eigen::Matrix2d::Random(), "Eigen::Matrix2d", 8, 24
+    Eigen::Matrix2d::Random(), "Eigen::Matrix2d",
+    matadd_flops(2, false, 2), matmul_flops(2, false, 2)
+  );
+  profile_for_type<Eigen::Matrix4d, Eigen::aligned_allocator>(
+    Eigen::Matrix4d::Random(), "Eigen::Matrix4d",
+    matadd_flops(4, false, 2), matmul_flops(4, false, 2)
+  );
+  profile_for_type<Eigen::Matrix2cd, Eigen::aligned_allocator>(
+    Eigen::Matrix2cd::Random(), "Eigen::Matrix2cd",
+    matadd_flops(2, true, 2), matmul_flops(2, true, 2)
+  );
+  profile_for_type<Eigen::Matrix3cd, Eigen::aligned_allocator>(
+    Eigen::Matrix3cd::Random(), "Eigen::Matrix3cd",
+    matadd_flops(3, true, 2), matmul_flops(3, true, 2)
   );
   return 0;
 }
--- lib/benchmarks/helpers.hpp
+++ lib/benchmarks/helpers.hpp
@@ -6,6 +6,22 @@
 #include <iostream>
 #include <chrono>
 
+unsigned int matmul_flops(
+  const unsigned int n, const bool complex, const unsigned int float_width)
+{
+  const unsigned int mul_flops = complex ? 6 : 1;
+  const unsigned int add_flops = complex ? 2 : 1;
+  return (n * mul_flops + (n - 1) * add_flops) * n * n * float_width;
+}
+
+
+unsigned int matadd_flops(
+  const unsigned int n, const bool complex, const unsigned int float_width)
+{
+  return n * n * float_width * (complex ? 2 : 1);
+}
+
+
 template <typename Fn>
 void benchmark(Fn func, const long num_flops = 0, const int num_trials = 100)
 {
