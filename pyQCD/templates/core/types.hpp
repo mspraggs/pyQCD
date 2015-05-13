375b76bf29eaeebc613bb66a12e400fe0b8538c3
Matt Spraggs <matthew.spraggs@gmail.com>
2015-05-14 00:56:25 +0100

    Fixed bad angled-bracket syntax in types.hpp

--- pyQCD/templates/core/types.hpp
+++ pyQCD/templates/core/types.hpp
@@ -14,7 +14,7 @@ typedef pyQCD::Lattice<pyQCD::Array<Complex> > LatticeComplexArray;
 {% for matrix in matrices %}
 typedef Eigen::Matrix<Complex, {{ matrix.num_rows }}, {{ matrix.num_cols }}> {{ matrix.matrix_name }};
 typedef pyQCD::MatrixArray<{{ matrix.num_rows }}, {{ matrix.num_cols }}, {{ precision }}> {{ matrix.array_name }};
-typedef pyQCD::Lattice<Eigen::Matrix<Complex, {{ matrix.num_rows }}, {{ matrix.num_cols }}, Eigen::aligned_allocator> {{ matrix.lattice_matrix_name }};
+typedef pyQCD::Lattice<Eigen::Matrix<Complex, {{ matrix.num_rows }}, {{ matrix.num_cols }}>, Eigen::aligned_allocator> {{ matrix.lattice_matrix_name }};
 typedef pyQCD::Lattice<pyQCD::MatrixArray<{{ matrix.num_rows }}, {{ matrix.num_cols }}, {{ precision }}> > {{ matrix.lattice_array_name }};
 {% endfor %}
 
