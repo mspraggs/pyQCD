264db9f5f3e1186585ad5db211c4b9a314d1a180
Matt Spraggs <matthew.spraggs@gmail.com>
2015-06-29 21:36:29 +0100

    Changed generate_type_node so it now just takes a list of functions

--- pyQCD/utils/codegen/nodegen.py
+++ pyQCD/utils/codegen/nodegen.py
@@ -6,12 +6,11 @@ from __future__ import absolute_import
 from Cython.Compiler import ExprNodes, Nodes
 
 
-def generate_type_node(typedef, builder):
-    """Iterate through features in typedef and build a Cython Node instance."""
+def generate_type_node(typedef, funcs):
+    """Iterate through builder functions and build a Cython Node instance."""
 
     stats = []
-    for feature in builder.features:
-        func = getattr(builder, "build_{}".format(feature))
+    for func in funcs:
         stats.append(func(typedef))
 
     return Nodes.CClassDefNode(None, class_name=typedef.name,
@@ -19,53 +18,40 @@ def generate_type_node(typedef, builder):
                                body=Nodes.StatListNode(None, stats=stats))
 
 
-class Builder(object):
-    """Handles Cython context and provides functions to build Cython nodes."""
+def build_instance(typedef, wrap_ptr=True):
+    """Create an instance attribute for cdef classes to wrap a C++ object"""
+    name_declarator = Nodes.CNameDeclaratorNode(None, name="instance")
+    if wrap_ptr:
+        declarators = [Nodes.CPtrDeclaratorNode(None, base=name_declarator)]
+    else:
+        declarators = [name_declarator]
+    return Nodes.CVarDefNode(
+        None, declarators=declarators,
+        base_type=Nodes.CSimpleBaseTypeNode(None, name=typedef.cname))
 
-    def __init__(self, wrap_ptr=True):
-        """Constructor for Builder. See help(Builder)."""
-        self.wrap_ptr = True
-        self.features = [attr[6:] for attr in dir(self)
-                         if attr.startswith("build_")]
-
-    def build_instance(self, typedef):
-        """Create an instance attribute for cdef classes to wrap a C++ object"""
-        name_declarator = Nodes.CNameDeclaratorNode(None, name="instance")
-        if self.wrap_ptr:
-            declarators = [Nodes.CPtrDeclaratorNode(None, base=name_declarator)]
-        else:
-            declarators = [name_declarator]
-        return Nodes.CVarDefNode(
-            None, declarators=declarators,
-            base_type=Nodes.CSimpleBaseTypeNode(None, name=typedef.cname))
-
-    def build_cppobj(self, typedef):
-        """Create cppobj member function to return instance"""
-        ret = ExprNodes.AttributeNode(None, attribute="instance",
-                                      obj=ExprNodes.NameNode(None, name="self"))
-        if self.wrap_ptr:
-            ret = ExprNodes.IndexNode(None,
-                                      index=ExprNodes.IntNode(None, value='0'),
-                                      base=ret)
-        declarator = Nodes.CFuncDeclaratorNode(
-            None, args=[
-                Nodes.CArgDeclNode(
-                    None, default=None,
-                    base_type=Nodes.CSimpleBaseTypeNode(None, name="self"),
-                    declarator=Nodes.CNameDeclaratorNode(None, name="")
-                )
-            ],
-            base=Nodes.CNameDeclaratorNode(None, name="cppobj")
-        )
-        return Nodes.CFuncDefNode(
-            None, overridable=False, visibility="private", api=0,
-            declarator=declarator,
-            body=Nodes.ReturnStatNode(None, value=ret),
-        )
+def build_cppobj(typedef, wrap_ptr=True):
+    """Create cppobj member function to return instance"""
+    ret = ExprNodes.AttributeNode(None, attribute="instance",
+                                  obj=ExprNodes.NameNode(None, name="self"))
+    if wrap_ptr:
+        ret = ExprNodes.IndexNode(None,
+                                  index=ExprNodes.IntNode(None, value='0'),
+                                  base=ret)
+    declarator = Nodes.CFuncDeclaratorNode(
+        None, args=[Nodes.CArgDeclNode(
+            None, default=None,
+            base_type=Nodes.CSimpleBaseTypeNode(None, name="self"),
+            declarator=Nodes.CNameDeclaratorNode(None, name="")
+        )], base=Nodes.CNameDeclaratorNode(None, name="cppobj"))
+    return Nodes.CFuncDefNode(
+        None, overridable=False, visibility="private", api=0,
+        declarator=declarator, body=Nodes.ReturnStatNode(None, value=ret),
+    )
 
 
-class ContainerBuilder(Builder):
-    """Builder subclass for Container types."""
+def build_real_imag(typedef):
+    """Create functions to return real and imaginary types"""
 
-    def build_getitem(self, typedef):
-        pass
+
+def build_getitem(self, typedef):
+    pass
