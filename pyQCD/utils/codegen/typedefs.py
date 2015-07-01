d78d42a1789586151c7fa219c127fb75b3a97ba5
Matt Spraggs <matthew.spraggs@gmail.com>
2015-07-01 22:09:06 +0100

    Added a couple of useful properties to ContainerDef

--- pyQCD/utils/codegen/typedefs.py
+++ pyQCD/utils/codegen/typedefs.py
@@ -41,6 +41,31 @@ class ContainerDef(TypeDef):
             child_ndims = 0
         return self_ndims + child_ndims
 
+    @property
+    def buffer_shape_expr(self):
+        """Generates an expression describing the shape of the buffer"""
+        if self.is_static:
+            out = [ExprNodes.IntNode(None, value=str(s)) for s in self.shape]
+        else:
+            out = [self.size_expr]
+        try:
+            out.extend(self.element_type.buffer_shape_expr)
+        except AttributeError:
+            pass
+        return out
+
+    @property
+    def accessor_triplets(self):
+        """Generates a list of tuples of lengths and types for accessor checks
+        """
+        out = [(self.ndims_expr, self.element_type.name,
+                self.element_type.cname)]
+        try:
+            out.extend(self.element_type.accessor_triplets)
+        except AttributeError:
+            pass
+        return out
+
 
 class MatrixDef(ContainerDef):
     """Specialise container definition for matrix type"""
