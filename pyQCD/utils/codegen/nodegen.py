bda06c7e4e4fdc8205c4acf5966ff362c3909a52
Matt Spraggs <matthew.spraggs@gmail.com>
2015-07-03 01:16:13 +0100

    Added assignment to out variable in build_getitem

--- pyQCD/utils/codegen/nodegen.py
+++ pyQCD/utils/codegen/nodegen.py
@@ -109,20 +109,31 @@ class ContainerBuilder(Builder):
         clauses = []
         len_sum_expr = None
         int_len_sum = 0
+        cobj = typedef.instance_val_accessor("out")
+        parent_type = typedef
+        parent_sum_expr = ExprNodes.IntNode(None, value='0')
         # Loop through num_dims and TypeDef descriptors
         # l is expression node giving the length of the tuple required
         # in order to return the type specified by TypeDef t.
-        for l, t in typedef.accessor_info:
-            lengths = generate_lookup_lengths(int_len_sum, len_sum_expr, l)
+        for ndims, elemtype in typedef.accessor_info:
+            lengths = generate_lookup_lengths(int_len_sum, len_sum_expr, ndims)
             int_len_sum, len_sum_expr, total_sum_expr = lengths
             condition = parse_string("type(index) is tuple and "
                                      "len(index) is x").expr
             condition.operand2.operand2 = total_sum_expr
             if_body = parse_string("out = Foo()\n"
                                    "return out")
-            if_body.stats[0].rhs.function.name = t.name
+            if_body.stats[0].rhs.function.name = elemtype.name
+
+            cobj = parent_type.ctype_elem_access(
+                cobj, ExprNodes.NameNode(None, name="index"), parent_sum_expr)
+            assignment = Nodes.SingleAssignmentNode(
+                None, lhs=typedef.instance_val_accessor("out"), rhs=cobj)
+            if_body.stats.insert(1, assignment)
             clauses.append(Nodes.IfClauseNode(None, condition=condition,
                                               body=if_body))
+            parent_type = elemtype
+            parent_sum_expr = total_sum_expr
         body = Nodes.IfStatNode(None, if_clauses=clauses, else_clause=None)
         return Nodes.DefNode(None, body=body, name="__getitem__",
                              args=generate_simple_args("self", "index"))
