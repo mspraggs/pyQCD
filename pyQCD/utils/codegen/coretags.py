a9216fd3e56d6f816b155989a49433d2dc2e5c62
Matt Spraggs <matthew.spraggs@gmail.com>
2015-07-07 16:57:03 +0100

    Fixed coretags docstring

--- pyQCD/utils/codegen/coretags.py
+++ pyQCD/utils/codegen/coretags.py
@@ -1,2 +1,2 @@
-"""This module contains functions for generating code snippets within the pxd
-and cpp source templates."""
+"""This module contains functions for generating attribute and member function
+code for each of the core types in core.pyx"""
