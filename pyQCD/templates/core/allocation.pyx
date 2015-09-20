3c780c651c97cedf9c709552b1df27aaf9e490d5
Matt Spraggs <matthew.spraggs@gmail.com>
2015-09-20 21:40:17 +0100

    Removed init function from allocation.pyx

--- pyQCD/templates/core/allocation.pyx
+++ pyQCD/templates/core/allocation.pyx
@@ -1,10 +1,11 @@
     cdef {{typedef.cmodule }}.{{ typedef.cname }}* instance
     cdef Layout layout
 
-{% for funcname in ["cinit", "init"] %}
-    def __{{ funcname }}__(self, {{ argstring }}):
+    def __cinit__(self, {{ argstring }}):
         self.instance = new {{ constructor_call }}
+{% if "layout" in argstring %}
+        self.layout = layout
+{% endif %}
 
-{% endfor %}
     def __dealloc__(self):
         del self.instance
