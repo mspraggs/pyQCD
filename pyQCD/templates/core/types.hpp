1ce13332c0ec64690ffe224043bc002f1cd635fa
Matt Spraggs <matthew.spraggs@gmail.com>
2015-09-20 21:42:25 +0100

    Updated types.hpp

--- pyQCD/templates/core/types.hpp
+++ pyQCD/templates/core/types.hpp
@@ -1,17 +1,17 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-#include "array.hpp"
-#include "matrix_array.hpp"
+#include <complex>
+
+#include <Eigen/Dense>
+
 #include "lattice.hpp"
 #include "constants.hpp"
 
 
 typedef {{ precision }} Real;
 typedef std::complex<Real> Complex;
-typedef pyQCD::Array<Complex> ComplexArray;
 typedef pyQCD::Lattice<Complex> LatticeComplex;
-typedef pyQCD::Lattice<pyQCD::Array<Complex> > LatticeComplexArray;
 {% for typedef in typedefs %}
 typedef {{ typedef|cpptype }} {{ typedef.cname }};
 {% endfor %}
