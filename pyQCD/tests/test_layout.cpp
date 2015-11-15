3173c51756a2001e4462ec8dd8ad195a720d85ca
Matt Spraggs <matthew.spraggs@gmail.com>
2015-11-15 22:50:59 +0000

    Added test for new Layout::subset function

--- pyQCD/tests/test_layout.cpp
+++ pyQCD/tests/test_layout.cpp
@@ -5,16 +5,28 @@
 #include "helpers.hpp"
 
 
+
 TEST_CASE("LexicoLayout test") {
-  pyQCD::LexicoLayout layout(std::vector<unsigned int>{8, 4, 4, 4});
+  typedef pyQCD::LexicoLayout Layout;
+
+  Layout layout({8, 4, 4, 4});
+  auto subset = layout.subset<Layout>(
+    [] (const Layout::Int i)
+    {
+      return (i % 2) ? false : true;
+    });
 
   for (int i = 0; i < 512; ++i) {
     REQUIRE (layout.get_array_index(i) == i);
     REQUIRE (layout.get_site_index(i) == i);
   }
-  REQUIRE (layout.get_array_index(std::vector<unsigned int>{4, 3, 2, 1})
+  REQUIRE (layout.get_array_index(std::vector<Layout::Int>{4, 3, 2, 1})
              == 313);
   REQUIRE (layout.volume() == 512);
   REQUIRE (layout.num_dims() == 4);
   REQUIRE ((layout.shape() == std::vector<unsigned int>{8, 4, 4, 4}));
+
+  for (int i = 0; i < 256; ++i) {
+    REQUIRE (subset.get_site_index(i) == 2 * i);
+  }
 }
