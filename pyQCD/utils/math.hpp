9a0e0276b889ce88a0b9c911e6dde6a30e91effb
Matt Spraggs <matthew.spraggs@gmail.com>
2016-01-13 13:40:55 +0000

    Added some constants to math.hpp

--- pyQCD/utils/math.hpp
+++ pyQCD/utils/math.hpp
@@ -3,8 +3,14 @@
 
 /* This file provides basic mathematics functions */
 
+#include <core/types.hpp>
+
+
 namespace pyQCD
 {
+  const Real pi = 3.14159265358979323846264338327950288419716939937510;
+  const Complex I(0.0, 1.0);
+
   int mod(const int i, const int n);
 }
 
