d71dbb0193d9c4cbf9d0366fbb9eb96341a635e4
Matt Spraggs <matthew.spraggs@gmail.com>
2016-01-14 00:28:25 +0000

    Added some commenting to heatbath.cpp SU(2) matrix generation

--- pyQCD/algorithms/heatbath.cpp
+++ pyQCD/algorithms/heatbath.cpp
@@ -9,18 +9,27 @@ namespace pyQCD {
 
   Matrix2cd gen_heatbath_su2(const Real weight, const Real beta)
   {
-    // Generate a random SU(2) matrix distributed according to the requirements
-    // of the heatbath algorithm. (See page 87 of Gattringer and Lang for the
-    // source material for this algorithm.)
+    // Generate a random SU(2) matrix distributed according to the distribution
+    // exp(0.5 * weight * beta * Re tr(X)). We use the algorithm specified in
+    // Kennedy and Pendleton (1985), in Phys. Lett. 156B.
+    //
+    // (See also page 87 of Gattringer and Lang for the source material for
+    // this algorithm.)
 
     typedef Eigen::Matrix<Real, 4, 1> FourVector;
 
+    // Random number generator setup
+    // TODO: Implement a proper random number generator
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
+    // Coefficients for the SU(2) basis of Pauli matrices. This final vector
+    // will need to be normalised.
     FourVector coeffs;
-
+    // Now we need to fill the components of this vector. The first component
+    // must be distributed according to
+    //   sqrt(1 - x^2) * exp(weight * beta * x)
     Real lambda_squared = 2.0;
     Real uniform_squared = std::pow(dis(gen), 2);
     while (uniform_squared > 1 - lambda_squared) {
@@ -29,14 +38,15 @@ namespace pyQCD {
       Real r2 = 1.0 - dis(gen);
       lambda_squared
         = - 1.0 / (2.0 * weight * beta)
-        * (std::log(r0)
-           + std::pow(std::cos(2 * pi * r1), 2) * std::log(r2));
+        * (std::log(r0) + std::pow(std::cos(2 * pi * r1), 2) * std::log(r2));
       uniform_squared = std::pow(dis(gen), 2);
     }
-
     coeffs[0] = 1 - 2 * lambda_squared;
+    // With the first component determined, the magnitude of the remaining
+    // three-vector can easily be determined.
     Real three_vec_magnitude = std::sqrt(1 - coeffs[0] * coeffs[0]);
-
+    // The remaining three-vector should then be take from a uniform spherical
+    // distribution.
     Real cos_theta = 2.0 * dis(gen) - 1.0;
     Real sin_theta = std::sqrt(1 - cos_theta * cos_theta);
     Real phi = 2 * pi * dis(gen);
