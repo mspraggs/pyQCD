03d55a1bb89b586a38f06c31ddee245417c312e9
Matt Spraggs <matthew.spraggs@gmail.com>
2016-01-16 18:01:38 +0000

    Updated heatbath tests to use new templates

--- pyQCD/tests/test_heatbath.cpp
+++ pyQCD/tests/test_heatbath.cpp
@@ -7,8 +7,10 @@
 
 TEST_CASE("Heatbath test")
 {
+  typedef double Real;
+
   Compare<Real> comp(1.0e-5, 1.0e-8);
-  MatrixCompare<SU2Matrix> mat_comp(1.0e-5, 1.0e-8);
+  MatrixCompare<pyQCD::SU2Matrix<Real>> mat_comp(1.0e-5, 1.0e-8);
 
   const unsigned int n = 10000;
   std::vector<Real> x0s(n);
@@ -16,7 +18,7 @@ TEST_CASE("Heatbath test")
     auto heatbath_su2 = pyQCD::gen_heatbath_su2(1.0, 5.0);
 
     REQUIRE(mat_comp(heatbath_su2 * heatbath_su2.adjoint(),
-      SU2Matrix::Identity()));
+      pyQCD::SU2Matrix<Real>::Identity()));
     auto det = heatbath_su2.determinant();
     REQUIRE(comp(det.real(), 1.0));
     REQUIRE(comp(det.imag(), 0.0));
