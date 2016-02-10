0952e4f746f14a3a6c37a11f83d592b221dd3a74
Matt Spraggs <matthew.spraggs@gmail.com>
2016-02-10 20:17:55 +0000

    Changed pyQCD::mod function to be templated

--- pyQCD/utils/math.hpp
+++ pyQCD/utils/math.hpp
@@ -31,7 +31,12 @@ namespace pyQCD
   const double pi = 3.14159265358979323846264338327950288419716939937510;
   const std::complex<double> I(0.0, 1.0);
 
-  int mod(const int i, const int n);
+  template <typename T>
+  T mod(const T i, const T n)
+  {
+    // Computes positive remainder of i divided by n.
+    return (i % n + n) % n;
+  }
 }
 
 #endif
