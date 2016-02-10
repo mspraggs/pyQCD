43aa4f0c0552ca8c5b9c8a7a14e7ca9d79aa4982
Matt Spraggs <matthew.spraggs@gmail.com>
2016-02-10 18:10:36 +0000

    Moved some of the code in the Layout.hpp into a source file

--- pyQCD/core/layout.hpp
+++ pyQCD/core/layout.hpp
@@ -54,23 +54,7 @@ namespace pyQCD
     typedef std::function<Int(const Int)> ArrFunc;
     typedef std::function<bool(const Int)> SubsetFunc;
 
-    Layout() = default;
-    Layout(const Site& shape, const ArrFunc& compute_array_index)
-      : num_dims_(static_cast<Int>(shape.size())),
-        shape_(shape)
-    {
-      // Constructor create arrays of site/array indices
-      volume_ = std::accumulate(shape.begin(), shape.end(), 1u,
-        std::multiplies<Int>());
-
-      array_indices_.resize(volume_);
-      site_indices_.resize(volume_);
-      for (Int site_index = 0; site_index < volume_; ++site_index) {
-        Int array_index = compute_array_index(site_index);
-        array_indices_[site_index] = array_index;
-        site_indices_[array_index] = site_index;
-      }
-    }
+    Layout(const Site& shape, const ArrFunc& compute_array_index);
     virtual ~Layout() = default;
 
     // Functions to retrieve array indices and so on.
@@ -123,7 +107,7 @@ namespace pyQCD
   {
     // Compute the lexicographic index of the specified site and use it to
     // to get the array index (coordinate at site[0] varies slowest, that at
-    // site[ndim - 1] varies fastest
+    // site[ndim - 1] varies fastest.
     int site_index = 0;
     for (Int i = 0; i < num_dims_; ++i) {
       site_index *= shape_[i];
@@ -138,8 +122,8 @@ namespace pyQCD
     auto site_index_copy = site_index;
     Site ret(num_dims_);
     for (int i = num_dims_ - 1; i > -1; --i) {
-      ret[i] = mod(site_index_copy, shape_[i]);
-      site_index_copy /= shape_[i];
+      ret[i] = mod(site_index_copy, global_shape_[i]);
+      site_index_copy /= global_shape_[i];
     }
     return ret;
   }
@@ -151,43 +135,6 @@ namespace pyQCD
     for (unsigned i = 0; i < num_dims_; ++i) {
       coords[i] = mod(coords[i], shape_[i]);
     }
-  }
-
-  template <typename T>
-  T Layout::subset(const SubsetFunc& func) const
-  {
-    // Create a layout for a subset of the sites contained within this instance
-    Layout ret;
-    Int j = 0;
-    for (unsigned int i = 0; i < site_indices_.size(); ++i) {
-      if (func(site_indices_[i])) {
-        ret.site_indices_.push_back(site_indices_[i]);
-        ret.array_indices_.push_back(j);
-        j++;
-      }
-    }
-    ret.volume_ = ret.site_indices_.size();
-    return *static_cast<T*>(&ret);
-  }
-
-
-  template <typename T>
-  T Layout::subset(const SubsetFunc& func, const Int size) const
-  {
-    // Create a layout for a subset of the sites contained within this instance
-    Layout ret;
-    ret.site_indices_.resize(size);
-    ret.array_indices_.resize(size);
-    ret.volume_ = size;
-    unsigned int j = 0;
-    for (unsigned int i = 0; i < site_indices_.size(); ++i) {
-      if (func(site_indices_[i])) {
-        ret.site_indices_[j] = site_indices_[i];
-        ret.array_indices_[j] = j;
-        j++;
-      }
-    }
-    return *static_cast<T*>(&ret);
   }
 }
 
--- pyQCD/tests/test_layout.cpp
+++ pyQCD/tests/test_layout.cpp
@@ -32,11 +32,6 @@ TEST_CASE("LexicoLayout test") {
   typedef pyQCD::LexicoLayout Layout;
 
   Layout layout({8, 4, 4, 4});
-  auto subset = layout.subset<Layout>(
-    [] (const pyQCD::Int i)
-    {
-      return (i % 2) ? false : true;
-    });
 
   for (int i = 0; i < 512; ++i) {
     REQUIRE (layout.get_array_index(i) == i);
@@ -47,8 +42,4 @@ TEST_CASE("LexicoLayout test") {
   REQUIRE (layout.volume() == 512);
   REQUIRE (layout.num_dims() == 4);
   REQUIRE ((layout.shape() == pyQCD::Site{8, 4, 4, 4}));
-
-  for (int i = 0; i < 256; ++i) {
-    REQUIRE (subset.get_site_index(i) == 2 * i);
-  }
 }
