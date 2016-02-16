93f030500748d77b3d6443df60719d7ea11203c4
Matt Spraggs <matthew.spraggs@gmail.com>
2016-02-16 16:48:11 +0000

    Added some useful(?) layout-related functions to use in Layout initialisation

---
+++ pyQCD/core/detail/layout_helpers.cpp
@@ -0,0 +1,134 @@
+/*
+ * This file is part of pyQCD.
+ * 
+ * pyQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * pyQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *
+ * Created by Matt Spraggs on 13/02/16.
+ *
+ *
+ * [DOCUMENTATION HERE]
+ */
+#include "layout_helpers.hpp"
+
+namespace pyQCD
+{
+  namespace detail
+  {
+    SiteIterator::SiteIterator(const Site& shape, const Site& coord,
+                               const Int index)
+      : num_dims_(static_cast<Int>(shape.size())), index_(index), shape_(shape),
+        current_site_(coord)
+    {
+      volume_ = compute_volume(shape.begin(), shape.size());
+    }
+
+    SiteIterator::SiteIterator(const Site& shape)
+      : SiteIterator(shape, Site(shape.size(), 0), 0)
+    { }
+
+    SiteIterator& SiteIterator::operator++()
+    {
+      // Increment by one lexicographic site (last site coordinate varies
+      // fastest).
+      Int i = num_dims_ - 1;
+      current_site_[i]++;
+      index_++;
+      while (current_site_[i] == shape_[i] and i != 0) {
+        current_site_[i--] = 0;
+        current_site_[i]++;
+      }
+      return *this;
+    }
+
+    template<>
+    std::vector <Site> get_all_sites(SiteIterator& iter)
+    {
+      // Generate a std::vector containing all the sites in the iterator
+      std::vector <Site> ret(iter.volume(), Site(iter.num_dims()));
+      for (Int i = 0; i < iter.volume(); ++i) {
+        ret[i] = *iter;
+        ++iter;
+      }
+      return ret;
+    }
+
+    template<>
+    std::vector <Eigen::VectorXi> get_all_sites(SiteIterator& iter)
+    {
+      // Generate a vector of Eigen vectors from the sites in the iterator
+      typedef Eigen::VectorXi IVec;
+      typedef Eigen::Matrix<Int, -1, 1> UVec;
+      std::vector <IVec> ret(iter.volume(), IVec::Zero(iter.num_dims()));
+      for (Int i = 0; i < iter.volume(); ++i) {
+        auto& site = *iter;
+        ret[i] = UVec::Map(site.data(), site.size()).cast<int>();
+        ++iter;
+      }
+      return ret;
+    }
+
+    template<>
+    Eigen::MatrixXi get_all_sites(SiteIterator& iter)
+    {
+      // Generate a vector of Eigen vectors from the sites in the iterator
+      typedef Eigen::Matrix<Int, -1, 1> UVec;
+      Eigen::MatrixXi ret(iter.volume(), iter.num_dims());
+      for (Int i = 0; i < iter.volume(); ++i) {
+        auto& site = *iter;
+        ret.col(i) = UVec::Map(site.data(), site.size()).cast<int>();
+        ++iter;
+      }
+      return ret;
+    }
+
+    std::vector<Eigen::VectorXi> generate_mpi_offsets(
+      const Int max_mpi_hop, const Int ndims,
+      const std::vector<bool>& need_comms)
+    {
+      // Generate all possible Cartesian coordinate offsets for the specified
+      // number of dimensions and maximum number of mpi_hops.
+      Int num_offsets = 0;
+      for (Int hop = 1; hop < max_mpi_hop + 1; ++hop) {
+        // Here we use the formula
+        //   2^(n - m) * (n choose m),
+        // for the number of m-cubes on the boundaries of the n-cube layout.
+        num_offsets += choose(ndims, ndims - hop) * std::pow(2, ndims - hop);
+      }
+      PYQCD_TRACE
+
+      Site shape(ndims, 3);
+      for (Int i = 0; i < ndims; ++i) {
+        shape[i] = (need_comms[i]) ? 3 : 1;
+      }
+      PYQCD_TRACE
+      SiteIterator site_iter(shape);
+      std::vector<IVec> ret(num_offsets, IVec::Zero(ndims));
+      Int i = 0;
+      for (auto& site : site_iter) {
+        Int hops = std::accumulate(site.begin(), site.end(), 0u,
+                                   [] (const Int total, const Int n)
+                                   { return total + std::abs(n); });
+        if (hops > max_mpi_hop or hops == 0) {
+          continue;
+        }
+        IVec eig_site
+          = UVec::Map(site.data(), ndims).cast<int>() - IVec::Ones(ndims);
+        ret[i++] = eig_site - Eigen::VectorXi::Ones(ndims);
+      }
+      PYQCD_TRACE
+
+      return ret;
+    }
+  }
+}
---
+++ pyQCD/core/detail/layout_helpers.hpp
@@ -0,0 +1,150 @@
+#ifndef PYQCD_LAYOUT_HELPERS_HPP
+#define PYQCD_LAYOUT_HELPERS_HPP
+/*
+ * This file is part of pyQCD.
+ * 
+ * pyQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * pyQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *
+ * Created by Matt Spraggs on 13/02/16.
+ *
+ *
+ * [DOCUMENTATION HERE]
+ */
+
+#include <iostream>
+
+#include <Eigen/Dense>
+
+#include <utils/math.hpp>
+
+
+namespace pyQCD
+{
+  namespace detail
+  {
+    typedef unsigned int Int;
+    typedef std::vector<Int> Site;
+    typedef Eigen::VectorXi IVec;
+    typedef Eigen::Matrix<Int, -1, 1> UVec;
+
+    template<typename T, typename U>
+    auto compute_volume(const T& shape, const U ndims)
+      -> typename std::remove_const<
+           typename std::remove_reference<decltype(shape[0])>::type
+         >::type
+    {
+      // Compute volume of the specified shape
+      typedef typename std::remove_const<
+        typename std::remove_reference<decltype(shape[0])>::type
+      >::type Elem;
+      return std::accumulate(shape, shape + ndims, Elem(1),
+                             std::multiplies<Elem>());
+    }
+
+    class SiteIterator
+    {
+      // Little iterator to loop over all sites on a lattice.
+      SiteIterator(const Site& shape, const Site& coord, const Int index);
+
+    public:
+      template<typename T>
+      SiteIterator(const T& shape, Int num_dims);
+
+      SiteIterator(const Site& shape);
+
+      bool operator!=(const SiteIterator& other) const
+      { return index_ != other.volume_; }
+
+      SiteIterator& operator++();
+
+      const Site& operator*() const { return current_site_; }
+
+      SiteIterator begin()
+      { return (index_ == 0) ? *this : SiteIterator(shape_); }
+      SiteIterator end()
+      { return SiteIterator(shape_, shape_, volume_); }
+
+      Int volume() const { return volume_; }
+      Int num_dims() const { return num_dims_; }
+
+    private:
+      Int num_dims_;
+      Int volume_;
+      Int index_;
+      Site shape_;
+      Site current_site_;
+    };
+
+    template<typename T>
+    SiteIterator::SiteIterator(const T& iter, Int size)
+      : SiteIterator(Site(iter, iter + size))
+    { }
+
+
+    template<typename T>
+    T get_all_sites(SiteIterator& iter);
+
+    template<>
+    std::vector <Site> get_all_sites(SiteIterator& iter);
+
+    template<>
+    std::vector <Eigen::VectorXi> get_all_sites(SiteIterator& iter);
+
+    template<>
+    Eigen::MatrixXi get_all_sites(SiteIterator& iter);
+
+    template <typename T, typename U>
+    Int coords_to_lex_index(const T& site, const U& shape, const Int ndims)
+    {
+      // Compute lexicographic index corresponding to the cartesian coordinates
+      // of the given site.
+      Int site_index = 0;
+      for (Int i = 0; i < ndims; ++i) {
+        site_index *= shape[i];
+        site_index += site[i];
+      }
+      return site_index;
+    }
+
+    template <typename T>
+    Site lex_index_to_coords(const Int site_index, const T& shape,
+                             const Int ndims)
+    {
+      // Compute lexicographic index corresponding to the cartesian coordinates
+      // of the given site.
+      auto site_index_copy = site_index;
+      Site ret(ndims);
+      for (int i = ndims - 1; i > -1; --i) {
+        ret[i] = site_index_copy % shape[i];
+        site_index_copy /= shape[i];
+      }
+      return ret;
+    }
+
+    template <typename T, typename U>
+    void sanitise_coords(T& coord, const U& shape, const Int ndims)
+    {
+      // Apply in-place periodic BCs to Cartesian coordinates.
+      for (int i = 0; i < static_cast<int>(ndims); --i) {
+        coord[i] = mod(coord[i], shape[i]);
+      }
+    }
+
+    std::vector<Eigen::VectorXi> generate_mpi_offsets(
+      const Int max_mpi_hop, const Int ndims,
+      const std::vector<bool>& need_comms);
+  }
+}
+
+#endif //PYQCD_LAYOUT_HELPERS_HPP
