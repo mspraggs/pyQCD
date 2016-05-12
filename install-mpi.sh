9f81567688d1aae7f251fc15981023868f479b6b
Matt Spraggs <matthew.spraggs@gmail.com>
2016-05-11 23:34:20 -0400

    Another attempt at a travis fix

---
+++ install-mpi.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+MPI_IMPL="$1"
+
+case "$MPI_IMPL" in
+  openmpi16)
+    if [ ! -d "$HOME/.local/$MPI_IMPL/bin" ]
+    then
+      echo "Installing OpenMPI 1.6.5"
+      wget https://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz
+      tar xvf openmpi-1.6.5.tar.gz
+      mkdir -p openmpi-1.6.5/build
+      cd openmpi-1.6.5/build
+      ../configure --prefix="$HOME/.local/$MPI_IMPL" && make && make install
+    else
+      echo "Using cached OpenMPI 1.6.5"
+    fi
+    ;;
+  *)
+    echo "Unknown MPI implementation"
+esac
+
+export PATH=$PATH:$HOME/.local/$MPI_IMPL/bin
+export LIBRARY_PATH=$LIBRARY_PATH:$HOME/.local/$MPI_IMPL/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/$MPI_IMPL/lib
