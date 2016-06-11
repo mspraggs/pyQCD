5dc40982cd7295778aefd16c7b2f10292c18382f
Matt Spraggs <matthew.spraggs@gmail.com>
2016-06-11 19:56:14 +0100

    Gauge: Updated WilsonAction to use site_size variable

--- pyQCD/gauge/wilson_action.hpp
+++ pyQCD/gauge/wilson_action.hpp
@@ -38,7 +38,7 @@ namespace pyQCD
 
       typename Action<Real, Nc>::GaugeLink compute_staples(
         const typename Action<Real, Nc>::GaugeField& gauge_field,
-        const Int site_index) const;
+        const Int link_index) const;
 
       Real local_action(
         const typename Action<Real, Nc>::GaugeField& gauge_field,
@@ -53,73 +53,77 @@ namespace pyQCD
       : Action<Real, Nc>(beta, layout)
     {
       // Determine which link indices belong to which link staples
-      links_.resize(layout.volume());
+      auto num_dims = layout.num_dims();
+      links_.resize(layout.volume() * num_dims);
       for (unsigned site_index = 0; site_index < layout.volume(); ++site_index)
       {
         Site link_coords = layout.compute_site_coords(site_index);
-        auto num_dims = layout.num_dims() - 1;
 
-        // Determine which plane the link does not contribute to.
-        Site planes(num_dims - 1);
-        int j = 0;
-        for (unsigned i = 0; i < num_dims; ++i) {
-          if (link_coords[num_dims] != i) {
-            planes[j++] = i;
+        for (Int d = 0; d < num_dims; ++d) { // d = dimension
+          Int link_index = site_index * num_dims + d;
+          // Determine which plane the link does not contribute to.
+          Site planes(num_dims - 1);
+          unsigned int j = 0;
+          for (unsigned int i = 0; i < num_dims; ++i) {
+            if (d != i) {
+              planes[j++] = i;
+            }
           }
-        }
 
-        links_[site_index].resize(6 * (num_dims - 1));
+          links_[link_index].resize(6 * (num_dims - 1));
 
-        for (unsigned i = 0; i < num_dims - 1; ++i) {
-          std::vector<int> link_coords_copy(
-            link_coords.begin(), link_coords.end());
+          for (unsigned i = 0; i < num_dims - 1; ++i) {
+            std::vector<int> link_coords_copy(
+              link_coords.begin(), link_coords.end());
 
-          /* First compute the loop above the link. Essentially we're interested
-           * in:
-           *
-           * U_\nu (x + \mu) U^\dagger_\mu (x + \nu) U^\dagger_\nu (x)
-           */
-          // First link in U_\nu (x + \mu)
-          link_coords_copy[num_dims] = planes[i];
-          link_coords_copy[link_coords[num_dims]]++;
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i] = layout.get_array_index(link_coords_copy);
-          // Next link is U^\dagger_\mu (x + \nu)
-          link_coords_copy[num_dims] = link_coords[num_dims];
-          link_coords_copy[link_coords[num_dims]]--;
-          link_coords_copy[planes[i]]++;
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i + 1]
-            = layout.get_array_index(link_coords_copy);
-          // Next link is U^\dagger_\nu (x)
-          link_coords_copy[planes[i]]--;
-          link_coords_copy[num_dims] = planes[i];
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i + 2]
-            = layout.get_array_index(link_coords_copy);
+            /* First compute the loop above the link. Essentially we're
+             * interested in:
+             *
+             * U_\nu (x + \mu) U^\dagger_\mu (x + \nu) U^\dagger_\nu (x)
+             */
+            // First link in U_\nu (x + \mu)
+            Int d_copy = planes[i];
+            link_coords_copy[d]++;
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i] = layout.get_array_index(
+              link_coords_copy) * num_dims + d_copy;
+            // Next link is U^\dagger_\mu (x + \nu)
+            d_copy = d;
+            link_coords_copy[d]--;
+            link_coords_copy[planes[i]]++;
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i + 1]
+              = layout.get_array_index(link_coords_copy) * num_dims + d_copy;
+            // Next link is U^\dagger_\nu (x)
+            link_coords_copy[planes[i]]--;
+            d_copy = planes[i];
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i + 2]
+              = layout.get_array_index(link_coords_copy) * num_dims + d_copy;
 
-          /* Now we want to compute the link below the link. Essentially:
-           *
-           * U^\dagger _\nu (x + \mu - \nu) U^\dagger_\mu (x - \nu)
-           *   U^\dagger_\nu (x - \nu)
-           */
-          // First link is U^\dagger_\nu (x + \mu - \nu)
-          link_coords_copy[link_coords[num_dims]]++;
-          link_coords_copy[planes[i]]--;
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i + 3]
-            = layout.get_array_index(link_coords_copy);
-          // Next link is U^\dagger_\mu (x - \nu)
-          link_coords_copy[num_dims] = link_coords_copy[num_dims];
-          link_coords_copy[link_coords[num_dims]]--;
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i + 4]
-            = layout.get_array_index(link_coords_copy);
-          // Next link is U_\nu (x - \nu)
-          link_coords_copy[num_dims] = planes[i];
-          layout.sanitize_site_coords(link_coords_copy);
-          links_[site_index][6 * i + 5]
-            = layout.get_array_index(link_coords_copy);
+            /* Now we want to compute the staple below the link. Essentially:
+             *
+             * U^\dagger _\nu (x + \mu - \nu) U^\dagger_\mu (x - \nu)
+             *   U^\dagger_\nu (x - \nu)
+             */
+            // First link is U^\dagger_\nu (x + \mu - \nu)
+            link_coords_copy[d]++;
+            link_coords_copy[planes[i]]--;
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i + 3]
+              = layout.get_array_index(link_coords_copy) * num_dims + d_copy;
+            // Next link is U^\dagger_\mu (x - \nu)
+            d_copy = d;
+            link_coords_copy[d]--;
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i + 4]
+              = layout.get_array_index(link_coords_copy) * num_dims + d_copy;
+            // Next link is U_\nu (x - \nu)
+            d_copy = planes[i];
+            layout.sanitize_site_coords(link_coords_copy);
+            links_[link_index][6 * i + 5]
+              = layout.get_array_index(link_coords_copy) * num_dims + d_copy;
+          }
         }
       }
     }
@@ -128,23 +132,23 @@ namespace pyQCD
     typename Action<Real, Nc>::GaugeLink
     WilsonAction<Real, Nc>::compute_staples(
       const typename Action<Real, Nc>::GaugeField& gauge_field,
-      const Int site_index) const
+      const Int link_index) const
     {
       auto ret = Action<Real, Nc>::GaugeLink::Zero().eval();
       auto temp_colour_mat = ret;
 
-      auto num_dims = gauge_field.layout().num_dims() - 1;
+      auto num_dims = gauge_field.layout().num_dims();
 
       for (unsigned i = 0; i < 6 * (num_dims - 1); i += 6) {
-        temp_colour_mat = gauge_field[links_[site_index][i]];
-        temp_colour_mat *= gauge_field[links_[site_index][i + 1]].adjoint();
-        temp_colour_mat *= gauge_field[links_[site_index][i + 2]].adjoint();
+        temp_colour_mat = gauge_field[links_[link_index][i]];
+        temp_colour_mat *= gauge_field[links_[link_index][i + 1]].adjoint();
+        temp_colour_mat *= gauge_field[links_[link_index][i + 2]].adjoint();
 
         ret += temp_colour_mat;
 
-        temp_colour_mat = gauge_field[links_[site_index][i + 3]].adjoint();
-        temp_colour_mat *= gauge_field[links_[site_index][i + 4]].adjoint();
-        temp_colour_mat *= gauge_field[links_[site_index][i + 5]];
+        temp_colour_mat = gauge_field[links_[link_index][i + 3]].adjoint();
+        temp_colour_mat *= gauge_field[links_[link_index][i + 4]].adjoint();
+        temp_colour_mat *= gauge_field[links_[link_index][i + 5]];
 
         ret += temp_colour_mat;
       }
--- pyQCD/tests/test_wilson_action.cpp
+++ pyQCD/tests/test_wilson_action.cpp
@@ -34,9 +34,9 @@ TEST_CASE("Test Wilson gauge action")
   SECTION("Testing computation of staples") {
     MatrixCompare<ColourMatrix> mat_comp(1.0e-8, 1.0e-8);
 
-    pyQCD::LexicoLayout layout({8, 4, 4, 4, 4});
+    pyQCD::LexicoLayout layout({8, 4, 4, 4});
     auto identity = ColourMatrix::Identity();
-    pyQCD::LatticeColourMatrix<double, 3> gauge_field(layout, identity);
+    pyQCD::LatticeColourMatrix<double, 3> gauge_field(layout, identity, 4);
     pyQCD::Gauge::WilsonAction<double, 3> action(5.0, layout);
 
     auto staple = action.compute_staples(gauge_field, 0);
@@ -46,9 +46,9 @@ TEST_CASE("Test Wilson gauge action")
     ColourMatrix rand_mat_1 = ColourMatrix::Random();
     ColourMatrix rand_mat_2 = ColourMatrix::Random();
     ColourMatrix rand_mat_3 = ColourMatrix::Random();
-    gauge_field(pyQCD::Site{1, 0, 0, 0, 1}) = rand_mat_1;
-    gauge_field(pyQCD::Site{0, 1, 0, 0, 0}) = rand_mat_2;
-    gauge_field(pyQCD::Site{0, 0, 0, 0, 1}) = rand_mat_3;
+    gauge_field(pyQCD::Site{1, 0, 0, 0}, 1) = rand_mat_1;
+    gauge_field(pyQCD::Site{0, 1, 0, 0}, 0) = rand_mat_2;
+    gauge_field(pyQCD::Site{0, 0, 0, 0}, 1) = rand_mat_3;
 
     staple = action.compute_staples(gauge_field, 0);
 
@@ -61,9 +61,9 @@ TEST_CASE("Test Wilson gauge action")
   SECTION("Testing local action") {
     Compare<Real> comp(1.0e-8, 1.0e-8);
 
-    pyQCD::LexicoLayout layout({8, 4, 4, 4, 4});
+    pyQCD::LexicoLayout layout({8, 4, 4, 4});
     auto identity = ColourMatrix::Identity();
-    pyQCD::LatticeColourMatrix<double, 3> gauge_field(layout, identity);
+    pyQCD::LatticeColourMatrix<double, 3> gauge_field(layout, identity, 4);
     pyQCD::Gauge::WilsonAction<double, 3> action(5.0, layout);
 
     Real local_action = action.local_action(gauge_field, 0);
