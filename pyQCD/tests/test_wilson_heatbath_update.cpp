668f06e826b0192fc2234d9197c27d203e73f2d1
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-13 17:15:24 +0000

    Tests: Added end-to-end test for heatbath update

---
+++ pyQCD/tests/test_wilson_heatbath_update.cpp
@@ -0,0 +1,62 @@
+/*
+ * This file is part of pyQCD.
+ *
+ * pyQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pyQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *
+ * Created by Matt Spraggs on 13/01/17.
+ *
+ * This file declares and defines the Lattice class, which is the fundamental
+ * class for representing variables on the lattice.
+ */
+
+#define CATCH_CONFIG_MAIN
+
+#include <algorithms/heatbath.hpp>
+#include <gauge/plaquette.hpp>
+#include <gauge/wilson_action.hpp>
+
+#include "helpers.hpp"
+
+
+TEST_CASE("End-to-end heatbath test with Wilson action")
+{
+  typedef pyQCD::Gauge::Action<double, 3>::GaugeField GaugeField;
+  typedef pyQCD::Gauge::Action<double, 3>::GaugeLink GaugeLink;
+
+  pyQCD::LexicoLayout layout({8, 8, 8, 8});
+  GaugeField gauge_field(layout, GaugeLink::Identity(), 4);
+
+  pyQCD::Gauge::WilsonAction<double, 3> action(5.5, layout);
+  pyQCD::Heatbath<double, 3> heatbath_updater(action);
+
+  double avg_plaquette = 1.1;
+  for (unsigned int i = 0; i < 10; ++i) {
+    heatbath_updater.update(gauge_field, 1);
+    double new_avg_plaquette
+        = pyQCD::Gauge::average_plaquette(gauge_field);
+    // Statistically it's highly probable there will be a monotonic decrease in
+    // the average plaquette over the first ten updates.
+    REQUIRE (new_avg_plaquette < avg_plaquette);
+    avg_plaquette = new_avg_plaquette;
+  }
+
+  for (unsigned int i = 0; i < 9; ++i) {
+    heatbath_updater.update(gauge_field, 10);
+  }
+
+  avg_plaquette = pyQCD::Gauge::average_plaquette(gauge_field);
+
+  REQUIRE(avg_plaquette < 0.51);
+  REQUIRE(avg_plaquette > 0.48);
+}
