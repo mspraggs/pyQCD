26fbfb680eb1529d10f8fbfad9987f73aa254d22
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-13 18:46:49 +0000

    Tests: Updated wilson heatbath test to check plaquette over five updates

--- pyQCD/tests/test_wilson_heatbath_update.cpp
+++ pyQCD/tests/test_wilson_heatbath_update.cpp
@@ -41,12 +41,12 @@ TEST_CASE("End-to-end heatbath test with Wilson action")
   pyQCD::Heatbath<double, 3> heatbath_updater(action);
 
   double avg_plaquette = 1.1;
-  for (unsigned int i = 0; i < 10; ++i) {
+  for (unsigned int i = 0; i < 5; ++i) {
     heatbath_updater.update(gauge_field, 1);
     double new_avg_plaquette
         = pyQCD::Gauge::average_plaquette(gauge_field);
     // Statistically it's highly probable there will be a monotonic decrease in
-    // the average plaquette over the first ten updates.
+    // the average plaquette over the first five updates.
     REQUIRE (new_avg_plaquette < avg_plaquette);
     avg_plaquette = new_avg_plaquette;
   }
