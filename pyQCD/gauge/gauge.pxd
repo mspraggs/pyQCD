383c567f6302ba0083178fb261716d6fe37e79da
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-18 14:32:56 +0000

    Cython: Exposed average_plaquette and average_rectangle functions

--- pyQCD/gauge/gauge.pxd
+++ pyQCD/gauge/gauge.pxd
@@ -1,4 +1,5 @@
 from pyQCD.core.atomics cimport Real
+from pyQCD.core.core cimport lattice_colour_matrix
 from pyQCD.core.layout cimport Layout
 
 
@@ -9,8 +10,15 @@ cdef extern from "types.hpp" namespace "pyQCD::python":
     cdef cppclass _WilsonGaugeAction "pyQCD::python::WilsonGaugeAction"(_GaugeAction):
         _WilsonGaugeAction(const Real, const Layout&) except +
 
+cdef extern from "gauge/plaquette.hpp" namespace "pyQCD::gauge":
+    cdef Real _average_plaquette "pyQCD::gauge::average_plaquette"(const lattice_colour_matrix.LatticeColourMatrix&) except +
+    
+cdef extern from "gauge/rectangle.hpp" namespace "pyQCD::gauge":
+    cdef Real _average_rectangle "pyQCD::gauge::average_rectangle"(const lattice_colour_matrix.LatticeColourMatrix&) except +
+
 cdef class GaugeAction:
     cdef _GaugeAction* instance
 
 cdef class WilsonGaugeAction(GaugeAction):
     pass
+
--- pyQCD/gauge/gauge.pyx
+++ pyQCD/gauge/gauge.pyx
@@ -1,17 +1,20 @@
 from pyQCD.core.core cimport LatticeColourMatrix
 from pyQCD.core.layout cimport Layout
 
-from gauge cimport GaugeAction, WilsonGaugeAction
+from gauge cimport GaugeAction, WilsonGaugeAction, _average_plaquette
 
 
 cdef class GaugeAction:
-
-    def __cinit__(self, float beta, gauge_field):
-        raise NotImplementedError("Cannot create empty gauge action. Use a "
-                                  "derived type.")
+    pass
 
 cdef class WilsonGaugeAction:
 
     def __cinit__(self, float beta, gauge_field):
         self.instance = new _WilsonGaugeAction(
             beta, (<LatticeColourMatrix?>gauge_field).lexico_layout[0])
+
+def average_plaquette(LatticeColourMatrix gauge_field):
+    return _average_plaquette(gauge_field.instance[0])
+
+def average_rectangle(LatticeColourMatrix gauge_field):
+    return _average_rectangle(gauge_field.instance[0])
