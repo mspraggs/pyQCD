195fb8993c419728b30ad8f2519283f63d10b658
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-21 16:38:28 +0000

    GaugeAction: Added raise of NotImplementedError to base class

--- pyQCD/gauge/gauge.pyx
+++ pyQCD/gauge/gauge.pyx
@@ -5,14 +5,20 @@ from gauge cimport GaugeAction, WilsonGaugeAction, _average_plaquette
 
 
 cdef class GaugeAction:
-    pass
+
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("The GaugeAction class should not be "
+                                  "instantiated directly. Instantiate a "
+                                  "derived class instead.")
 
 cdef class WilsonGaugeAction:
 
     def __cinit__(self, float beta, shape):
         cdef Layout* layout = new LexicoLayout(shape)
-        self.instance = new _WilsonGaugeAction(beta, layout[0]
-                                               )
+        self.instance = new _WilsonGaugeAction(beta, layout[0])
+
+    def __init__(self, *args, **kwargs):
+        pass
 
 def average_plaquette(LatticeColourMatrix gauge_field):
     return _average_plaquette(gauge_field.instance[0])
