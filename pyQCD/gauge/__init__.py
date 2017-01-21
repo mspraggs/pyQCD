25b55693df8dd7dc65d884564f3543ba876f4317
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-21 15:54:59 +0000

    Gauge: Added hot_start function

--- pyQCD/gauge/__init__.py
+++ pyQCD/gauge/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
-from pyQCD.core import LatticeColourMatrix
+import numpy as np
+
+from pyQCD.core import LatticeColourMatrix, random_colour_matrix
 from pyQCD.gauge.gauge import *
 
 
@@ -19,5 +21,27 @@ def cold_start(lattice_shape):
 
     for i in range(num_colours):
         ret_view[..., i, i] = 1.0
+
+    return ret
+
+def hot_start(lattice_shape):
+    """Creates an SU(N) gauge field with random links.
+
+    Arguments:
+      lattice_shape (iterable): The lattice shape the gauge field should
+        exhibit.
+
+    Returns:
+      LatticeColourMatrix: The hot-start gauge field.
+    """
+    # TODO: Move the below into Cython/C++ code for speed
+
+    lattice_shape = list(lattice_shape)
+    ret = LatticeColourMatrix(lattice_shape, len(lattice_shape))
+
+    ret_view = ret.as_numpy
+
+    for index in np.ndindex(*(lattice_shape + [len(lattice_shape)])):
+        ret_view[index] = random_colour_matrix().as_numpy
 
     return ret
--- pyQCD/tests/test_gauge.py
+++ pyQCD/tests/test_gauge.py
@@ -32,3 +32,21 @@ def test_cold_start():
 
     for index in np.ndindex(*(shape + (num_colours,))):
         assert np.allclose(array_view[index], id)
+
+
+def test_hot_start():
+    """Test construction of hot start gauge field"""
+
+    shape = (8, 4, 4, 4)
+    gauge_field = gauge.hot_start(shape)
+
+    num_colours = gauge_field.as_numpy.shape[-1]
+    id = np.identity(num_colours)
+
+    array_view = gauge_field.as_numpy
+
+    for index in np.ndindex(*(shape + (num_colours,))):
+        link = array_view[index]
+        assert np.allclose(np.linalg.det(link), 1.0)
+        assert np.allclose(np.dot(link, np.conj(link.T)), id)
+
