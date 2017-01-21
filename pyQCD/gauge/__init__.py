cca3c3eacf7f3499860d3f3384e3c0c091625b74
Matt Spraggs <matthew.spraggs@gmail.com>
2017-01-21 13:31:16 +0000

    Gauge: Added cold_start function with associated test

--- pyQCD/gauge/__init__.py
+++ pyQCD/gauge/__init__.py
@@ -1,3 +1,23 @@
 from __future__ import absolute_import
 
+from pyQCD.core import LatticeColourMatrix
 from pyQCD.gauge.gauge import *
+
+
+def cold_start(lattice_shape):
+    """Creates an SU(N) gauge field with each link set to the identity.
+
+    Arguments:
+      lattice_shape (iterable): The lattice shape the gauge field should
+        exhibit.
+    """
+    lattice_shape = list(lattice_shape)
+    ret = LatticeColourMatrix(lattice_shape, len(lattice_shape))
+
+    ret_view = ret.as_numpy
+    num_colours = ret_view.shape[-1]
+
+    for i in range(num_colours):
+        ret_view[..., i, i] = 1.0
+
+    return ret
--- pyQCD/tests/test_gauge.py
+++ pyQCD/tests/test_gauge.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import numpy as np
+
 from pyQCD import gauge
 
 
@@ -15,3 +17,18 @@ class TestWilsonGaugeAction(object):
     def test_constructor(self):
         """Test construction of WilsonGaugeAction"""
         action = gauge.WilsonGaugeAction(5.5, [8, 8, 8, 8])
+
+
+def test_cold_start():
+    """Test construction of cold start gauge field"""
+
+    shape = (8, 4, 4, 4)
+    gauge_field = gauge.cold_start(shape)
+
+    num_colours = gauge_field.as_numpy.shape[-1]
+    id = np.identity(num_colours)
+
+    array_view = gauge_field.as_numpy
+
+    for index in np.ndindex(*(shape + (num_colours,))):
+        assert np.allclose(array_view[index], id)
