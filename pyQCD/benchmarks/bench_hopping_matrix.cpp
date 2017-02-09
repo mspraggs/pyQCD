ce81c6798ef82e3a129891b2bd97b6672f002ea6
Matt Spraggs <matthew.spraggs@gmail.com>
2017-02-09 14:22:10 +0000

    Benchmarks: Added benchmark of HoppingMatrix::apply_full

---
+++ pyQCD/benchmarks/bench_hopping_matrix.cpp
@@ -0,0 +1,46 @@
+/*
+ * This file is part of pyQCD.
+ *
+ * pyQCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pyQCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *
+ * Created by Matt Spraggs on 09/02/17.
+ *
+ * Benchmark for HoppingMatrix::apply_full function.
+ */
+
+#include <fermions/hopping_matrix.hpp>
+
+#include "helpers.hpp"
+
+
+int main()
+{
+  pyQCD::LexicoLayout layout({8, 8, 8, 8});
+  pyQCD::LatticeColourMatrix<double, 3> gauge_field(layout, 4);
+  gauge_field.fill(pyQCD::ColourMatrix<double, 3>::Identity());
+
+  std::vector<Eigen::MatrixXcd>
+      spin_structures(8, Eigen::MatrixXcd::Identity(4, 4));
+
+  pyQCD::fermions::HoppingMatrix<double, 3, 1>
+      hopping_matrix(gauge_field, spin_structures);
+
+  pyQCD::LatticeColourVector<double, 3> fermion_in(layout, 4);
+  pyQCD::LatticeColourVector<double, 3> fermion_out(layout, 4);
+
+  std::cout << "Benchmarking HoppingMatrix::apply_full..." << std::endl;
+  benchmark([&] () {
+    hopping_matrix.apply_full(fermion_out, fermion_in);
+  }, 0, 100);
+}
